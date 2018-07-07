c4f3343713a650968ccbf553387f6b1b41aef2d3
Jack Tanner <jack@jackandtheblockstalk.com>
2018-07-07 21:29:17 +0100

    fixed typo in require

--- server/controller/users.controller.js
+++ server/controller/users.controller.js
@@ -1,7 +1,7 @@
 var config = require('config.json');
 var express = require('express');
 var router = express.Router();
-var userService = require('../servicesservices/user.service');
+var userService = require('../services/user.service');
 const uploadMulter = require('../services/uploadMulter');
 
 /******** routes ****************/
