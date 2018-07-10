e13a46b694eca25d6ab00d4914bb9877e095bff3
Jack Tanner <jack@jackandtheblockstalk.com>
2018-07-10 17:57:14 +0100

    changed FE endpoint url

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -4,15 +4,15 @@ declare var synapseThrow: any;
 import { Router, ActivatedRoute } from '@angular/router';
 import {UserService} from '../user.service';
 import {User} from '../Model/user';
-const URL = 'http://workonblockchain.mwancloud.com:4000/';
-//const URL = 'http://localhost:4000/';
+// const URL = 'http://workonblockchain.mwancloud.com:4000/';
+const URL = 'http://localhost:4000/';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit 
+export class AboutComponent implements OnInit
 {
   currentUser: User;
   log='';
@@ -20,18 +20,18 @@ export class AboutComponent implements OnInit
   link=''; class=''; resume_class;exp_class;final_class;googleUser;linkedinUser;active_class;
   job_active_class;
   exp_active_class;resume_active_class;
-  gender = 
+  gender =
   [
-    "Male", 
+    "Male",
     "Female"
-  ]  
+  ]
 
-  constructor(private http: HttpClient,private route: ActivatedRoute,private router: Router,private authenticationService: UserService, private el: ElementRef) 
-  { 
+  constructor(private http: HttpClient,private route: ActivatedRoute,private router: Router,private authenticationService: UserService, private el: ElementRef)
+  {
   }
 
 
-  ngOnInit() 
+  ngOnInit()
   {
        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
        console.log(this.currentUser);
@@ -47,12 +47,12 @@ export class AboutComponent implements OnInit
           this.info.first_name= this.googleUser.firstName;
           this.info.last_name = this.googleUser.lastName;
        }
-       
+
        if(!this.currentUser)
        {
           this.router.navigate(['/signup']);
        }
-       
+
        if(this.currentUser && this.currentUser.type=='candidate')
        {
           this.class="btn disabled";
@@ -61,11 +61,11 @@ export class AboutComponent implements OnInit
           this.final_class = "btn disabled";
            this.authenticationService.getById(this.currentUser._id)
             .subscribe(
-                data => 
+                data =>
                 {
                   console.log(data);
                   console.log(data._creator.social_type);
-                  
+
                   if(data.contact_number  && data.nationality && data.first_name && data.last_name)
                   {
                     this.active_class='fa fa-check-circle text-success';
@@ -86,24 +86,24 @@ export class AboutComponent implements OnInit
                     {
                         this.info.image_src = data.image ;
                      }
-                    
+
                     this.link= "/job";
-                    
+
                   }
-                 
+
                   if(data.country && data.roles && data.interest_area && data.expected_salary && data.availability_day )
                   {
                       this.job_active_class = 'fa fa-check-circle text-success';
-                       
+
                   }
-               
+
               if(data.commercial_platform && data.experimented_platform && data.why_work )
               {
                 this.resume_class="/resume";
                 this.resume_active_class='fa fa-check-circle text-success';
                // this.router.navigate(['/resume']);
               }
-     
+
               if(data.history && data.education && data.experience_roles && data.current_salary )
               {
                   this.exp_class = "/experience";
@@ -111,10 +111,10 @@ export class AboutComponent implements OnInit
                   //this.router.navigate(['/experience']);
               }
 
-              
-                  
+
+
                 },
-                error => 
+                error =>
                 {
                   this.log = 'Something getting wrong';
                 });
@@ -124,23 +124,23 @@ export class AboutComponent implements OnInit
        {
             this.router.navigate(['/not_found']);
        }
-       
+
   }
 
   onGenderSelected(event)
   {
     this.info.gender= event.target.value;
     console.log(this.info.gender);
-   
+
   }
-  
- 
+
+
   about()
   {
 
       this.authenticationService.about(this.currentUser._creator,this.info)
         .subscribe(
-          data => 
+          data =>
           {
             if(data)
             {
@@ -149,15 +149,15 @@ export class AboutComponent implements OnInit
               let inputEl: HTMLInputElement = this.el.nativeElement.querySelector('#aa');
               let fileCount: number = inputEl.files.length;
               let formData = new FormData();
-              if (fileCount > 0 ) 
-              { 
+              if (fileCount > 0 )
+              {
                 formData.append('photo', inputEl.files.item(0));
 
-                this.http.post(URL+'users/image/'+this.currentUser._creator, formData).map((res) => res).subscribe(                
-                (success) => 
+                this.http.post(URL+'users/image/'+this.currentUser._creator, formData).map((res) => res).subscribe(
+                (success) =>
                 {
                   console.log(success);
-                  this.router.navigate(['/job']); 
+                  this.router.navigate(['/job']);
                 },
                 (error) => alert(error))
               }
@@ -165,20 +165,20 @@ export class AboutComponent implements OnInit
               {
                 this.router.navigate(['/job']);
               }
-              
+
               }
               else if (this.info.image_src)
               {
                 this.router.navigate(['/job']);
               }
-              
+
             }
             if(data.error )
             {
               this.log=data.error;
             }
           },
-          error => 
+          error =>
           {
             this.log = 'Something getting wrong';
           });
