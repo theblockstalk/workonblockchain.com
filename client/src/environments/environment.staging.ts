1b6bdd5c9b8a26320ef9eec61262b522fe16d366
Sadia Abbas <sadia.abbas@mwanmobile.com>
2018-07-27 12:02:56 +0500

    Functional bugs fixed

--- client/src/environments/environment.staging.ts
+++ client/src/environments/environment.staging.ts
@@ -3,5 +3,6 @@ export const environment = {
   backend_url: 'https://staging-api.workonblockchain.com/',
   frontend_url: 'https://staging.workonblockchain.com/',
   google_id: '699068166900-h9dp76b47mh4biq33e8c31ju8tvfgvqj.apps.googleusercontent.com',
+  linkedin_id : '',
 };
 console.log(environment);
