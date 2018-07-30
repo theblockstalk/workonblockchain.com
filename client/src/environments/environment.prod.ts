ac084202b691874a04daff78e6eccd1286a8acd5
Tayyab Hussain <tayyab.hussain@mwanmobile.com>
2018-07-30 10:42:31 +0500

    Tayyab

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  backend_url: 'https://production-api.workonblockchain.com/',
-  frontend_url: 'https://workonblockchain.com/',
+  backend_url: 'http://workonblockchain.mwancloud.com:4000/',
+  frontend_url: 'http://workonblockchain.mwancloud.com/',
   google_id: '507151802069-sedtrf34188eet5oo4adrm60vlsruo5r.apps.googleusercontent.com',
   linkedin_id : '78axuc5uh894iq',
 
