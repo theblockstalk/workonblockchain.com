682b217b76b0bc9aece92888a1e9074bd6b7f4b6
Jack Tanner <jack@jackandtheblockstalk.com>
2018-08-14 16:02:27 +0100

    handling errors

--- scripts/client.js
+++ scripts/client.js
@@ -12,32 +12,34 @@ const tempClientDirName = './temp/client/dist/';
 const s3bucket = config.s3.frontendBucket.staging;
 
 async function deployFrontend() {
-    console.log('This script will deploy the latest in the /client directory to the frontend application');
-    console.log('Please make sure there is no files in the working directory (responsibly do a `git stash` if you are unsure)');
+    try {
+        console.log('This script will deploy the latest in the /client directory to the frontend application');
+        console.log('Please make sure there is no files in the working directory (responsibly do a `git stash` if you are unsure)');
 
-    await scriptUtils.pressEnterToContinue();
+        await scriptUtils.pressEnterToContinue();
 
-    console.log();
-    console.log('(1/4) getting Git branch and commit info');
-    const gitInfo = await scriptUtils.getGitCommit();
-    console.log(gitInfo);
+        console.log();
+        console.log('(1/4) getting Git branch and commit info');
+        const gitInfo = await scriptUtils.getGitCommit();
+        console.log(gitInfo);
 
-    scriptUtils.checkGitBranch(gitInfo.branch, 'staging');
+        scriptUtils.checkGitBranch(gitInfo.branch, 'staging');
 
-    console.log();
-    console.log('(2/4) building distribution in client/dist/');
-    await scriptUtils.pressEnterToContinue();
-    // TODO: add in some tags for application to serve the version commit
-    await scriptUtils.buildAngularDistribution();
+        console.log();
+        console.log('(2/4) building distribution in client/dist/');
+        await scriptUtils.pressEnterToContinue();
+        // TODO: add in some tags for application to serve the version commit
+        await scriptUtils.buildAngularDistribution();
 
-    console.log();
-    console.log('(3/4) moving to temporary directory temp/client/dist');
-    await scriptUtils.createTempClientDir(tempClientDirName);
+        console.log();
+        console.log('(3/4) moving to temporary directory temp/client/dist');
+        await scriptUtils.createTempClientDir(tempClientDirName);
 
-    console.log();
-    console.log('(4/4) syncing to S3 bucket');
-    await scriptUtils.pressEnterToContinue();
-    await scriptUtils.syncDirwithS3(s3bucket, tempClientDirName);
-
-
+        console.log();
+        console.log('(4/4) syncing to S3 bucket');
+        await scriptUtils.pressEnterToContinue();
+        await scriptUtils.syncDirwithS3(s3bucket, tempClientDirName);
+    } catch(error) {
+        console.log(error);
+    }
 }
--- scripts/server.js
+++ scripts/server.js
@@ -14,38 +14,42 @@ const envName = config.eb.envName.staging;
 const s3bucket = config.s3.distributionsBucket;
 
 async function deployBackend() {
-    console.log('This script will deploy the latest in the /server directory to the backend application');
-    console.log('Please make sure there is no files in the working directory (responsibly do a `git stash` if you are unsure)');
+    try {
+        console.log('This script will deploy the latest in the /server directory to the backend application');
+        console.log('Please make sure there is no files in the working directory (responsibly do a `git stash` if you are unsure)');
 
-    await scriptUtils.pressEnterToContinue();
+        await scriptUtils.pressEnterToContinue();
 
-    console.log();
-    console.log('(1/5) getting Git branch and commit info');
-    const gitInfo = await scriptUtils.getGitCommit();
-    console.log(gitInfo);
+        console.log();
+        console.log('(1/5) getting Git branch and commit info');
+        const gitInfo = await scriptUtils.getGitCommit();
+        console.log(gitInfo);
 
-    scriptUtils.checkGitBranch(gitInfo.branch, 'staging');
+        scriptUtils.checkGitBranch(gitInfo.branch, 'staging');
 
-    console.log();
-    console.log('(2/5) creating distribution package from server/');
-    await scriptUtils.createTempServerDir(tempServerDirName);
-    // TODO: add in some tags for application to serve the version commit
-    const zipFileName = await scriptUtils.zipServerDir(tempDirName, tempServerDirName, gitInfo.commit);
+        console.log();
+        console.log('(2/5) creating distribution package from server/');
+        await scriptUtils.createTempServerDir(tempServerDirName);
+        // TODO: add in some tags for application to serve the version commit
+        const zipFileName = await scriptUtils.zipServerDir(tempDirName, tempServerDirName, gitInfo.commit);
 
-    console.log(zipFileName);
-    console.log('(3/5) uploading the environment version (distribution) to S3');
-    await scriptUtils.pressEnterToContinue();
-    let distributionS3File = await scriptUtils.uploadZipfileToS3(envName, s3bucket, zipFileName);
-    console.log(distributionS3File);
+        console.log(zipFileName);
+        console.log('(3/5) uploading the environment version (distribution) to S3');
+        await scriptUtils.pressEnterToContinue();
+        let distributionS3File = await scriptUtils.uploadZipfileToS3(envName, s3bucket, zipFileName);
+        console.log(distributionS3File);
 
-    console.log();
-    console.log('(4/5) creating a new application version');
-    await scriptUtils.pressEnterToContinue();
-    await scriptUtils.addElasticEnvironmentVersion(s3bucket, appName, zipFileName, distributionS3File);
+        console.log();
+        console.log('(4/5) creating a new application version');
+        await scriptUtils.pressEnterToContinue();
+        await scriptUtils.addElasticEnvironmentVersion(s3bucket, appName, zipFileName, distributionS3File);
 
-    console.log();
-    console.log('(5/5) updating the elastic beanstalk environment');
-    await scriptUtils.pressEnterToContinue();
-    const ebUpdate = await scriptUtils.updateElisticEnvironment(appName, envName, zipFileName);
-    console.log(ebUpdate);
+        console.log();
+        console.log('(5/5) updating the elastic beanstalk environment');
+        await scriptUtils.pressEnterToContinue();
+        const ebUpdate = await scriptUtils.updateElisticEnvironment(appName, envName, zipFileName);
+        console.log(ebUpdate);
+    } catch(error) {
+        console.log(error);
+    }
 }
