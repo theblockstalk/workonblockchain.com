56ef58e43d13c2137200109d3d546f42ece3fe71
Sadia Abbas <sadia.abbas@mwanmobile.com>
2018-09-13 18:17:17 +0500

    add logger in referred friend

--- server/controller/services/email/emails/referredFriend.js
+++ server/controller/services/email/emails/referredFriend.js
@@ -1,4 +1,5 @@
 const emails = require('../emails');
+const logger = require('../../logger');
 
 module.exports.sendEmail = function sendEmail(data) {
 
@@ -7,7 +8,8 @@ module.exports.sendEmail = function sendEmail(data) {
     };
     const subject = data.info.referred_fname + ' has created a profile on Work on Blockchain!';
     const sendToArray = [sendTo];
-
+    logger.debug('refeered email: ' , data.info.email);
+    logger.debug('refeered email: ' , data.info.email);
     const mandrillOptions = {
         templateName: "wob-you-referred-a-user",
         message: {
