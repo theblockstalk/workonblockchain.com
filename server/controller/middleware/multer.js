f557e6bd86d9144276e4b72832854c9cf72b473b
Jack Tanner <jack@jackandtheblockstalk.com>
2018-10-02 18:44:20 +0100

    removed aws permissions from multer

--- server/controller/middleware/multer.js
+++ server/controller/middleware/multer.js
@@ -11,8 +11,8 @@ if (settings.ENVIRONMENT === 'production' || settings.ENVIRONMENT === 'staging')
     logger.info('Configuring multer with S3 bucket');
 
     aws.config.update({
-        secretAccessKey: settings.AWS.SECRET_ACCESS_KEY,
-        accessKeyId: settings.AWS.ACCESS_KEY,
+        // secretAccessKey: settings.AWS.SECRET_ACCESS_KEY,
+        // accessKeyId: settings.AWS.ACCESS_KEY,
         region: settings.AWS.REGION
     });
 
