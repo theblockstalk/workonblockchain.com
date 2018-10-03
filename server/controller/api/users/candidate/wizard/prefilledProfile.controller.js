d90278724f17fa6ff6d963f7a986a1d8ab4ad21d
Jack Tanner <jack@jackandtheblockstalk.com>
2018-10-03 13:17:02 +0100

    converted to async await

--- server/controller/api/users/candidate/wizard/prefilledProfile.controller.js
+++ server/controller/api/users/candidate/wizard/prefilledProfile.controller.js
@@ -1,69 +1,87 @@
-var Q = require('q');
-var mongo = require('mongoskin');
+// var Q = require('q');
+// var mongo = require('mongoskin');
 const CandidateProfile = require('../../../../../model/candidate_profile');
-const logger = require('../../../../services/logger');
+// const logger = require('../../../../services/logger');
+// const errors = require('../../../../services/errors');
 
 ///// for update the candidate profile data ///////////////////
 
-module.exports = function (req, res)
-{
-    let userId = req.auth.user._id;
+module.exports = async function (req, res) {
+    const userId = req.auth.user._id;
 
-    update_candidate_profile(userId, req.body).then(function (err, data)
-    {
-        if (data)
-        {
-            res.json(data);
-        }
-        else
-        {
-            res.send(err);
-        }
-    })
-        .catch(function (err)
-        {
-            res.json({error: err});
-        });
-}
+    const candidateDoc = await CandidateProfile.findOne({ _creator: userId }).lean();
 
-function update_candidate_profile(_id,userParam)
-{
-    var deferred = Q.defer();
-    var _id = _id;
+    const userParam = req.body;
+    const candidateUpdate = {
+        first_name: userParam.basics.first_name,
+        last_name: userParam.basics.last_name,
+        // @sadia Phone number also?
+        education_history: userParam.educationHistory,
+        work_history: userParam.workHistory,
+        description: userParam.basics.summary
+    };
 
-    CandidateProfile.findOne({ _creator: _id }, function (err, data)
-    {
-        if (err){
-            logger.error(err.message, {stack: err.stack});
-            deferred.reject(err.name + ': ' + err.message);
-        }
-        else
-            updateUser(_id);
+    await CandidateProfile.update({ _id: candidateDoc._id },{ $set: candidateUpdate });
 
-    });
+    res.send();
+};
 
-    function updateUser(_id)
-    {
-        var set =
-            {
-                first_name:userParam.basics.first_name,
-                last_name:userParam.basics.last_name,
-                education_history :  userParam.educationHistory,
-                work_history: userParam.workHistory,
-                description :userParam.basics.summary
+// {
+//     update_candidate_profile(userId, req.body).then(function (err, data)
+//     {
+//         if (data)
+//         {
+//             res.json(data);
+//         }
+//         else
+//         {
+//             res.send(err);
+//         }
+//     })
+//         .catch(function (err)
+//         {
+//             res.json({error: err});
+//         });
+// }
 
-            };
-
-        CandidateProfile.update({ _creator: mongo.helper.toObjectID(_id) },{ $set: set },function (err, doc)
-        {
-            if (err){
-                logger.error(err.message, {stack: err.stack});
-                deferred.reject(err.name + ': ' + err.message);
-            }
-            else
-                deferred.resolve(set);
-        });
-    }
-
-    return deferred.promise;
-}
+// function update_candidate_profile(_id,userParam)
+// {
+//     var deferred = Q.defer();
+//     var _id = _id;
+//
+//     CandidateProfile.findOne({ _creator: _id }, function (err, data)
+//     {
+//         if (err){
+//             logger.error(err.message, {stack: err.stack});
+//             deferred.reject(err.name + ': ' + err.message);
+//         }
+//         else
+//             updateUser(_id);
+//
+//     });
+//
+//     function updateUser(_id)
+//     {
+//         var set =
+//             {
+//                 first_name:userParam.basics.first_name,
+//                 last_name:userParam.basics.last_name,
+//                 education_history :  userParam.educationHistory,
+//                 work_history: userParam.workHistory,
+//                 description :userParam.basics.summary
+//
+//             };
+//
+//         CandidateProfile.update({ _creator: mongo.helper.toObjectID(_id) },{ $set: set },function (err, doc)
+//         {
+//             if (err){
+//                 logger.error(err.message, {stack: err.stack});
+//                 deferred.reject(err.name + ': ' + err.message);
+//             }
+//             else
+//                 deferred.resolve(set);
+//         });
+//     }
+//
+//     return deferred.promise;
+// }
