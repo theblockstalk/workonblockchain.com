bcbea0e42345b7a2cfcdbe83828a4e4b9b6589f6
Sadia Abbas <sadia.abbas@mwanmobile.com>
2018-10-12 11:59:22 +0500

    changed in schema

--- server/model/enumerations.js
+++ server/model/enumerations.js
@@ -1,6 +1,6 @@
 
 // TODO: complete this
-module.exports.blockchainPlatforms = ['Bitcoin', 'Ethereum', 'Ripple','Stellar', 'Hyperledger Fabric','Hyperledger Sawtooth','Quorum','Corda','EOS','NEO','Waves','Steem','Lisk','Quantum','Tezos','Cardano','Litecoin','Monero','ZCash','IOTA','NEM','NXT','Dash','Doge'];
+module.exports.blockchainPlatforms = ['Bitcoin', 'Ethereum', 'Ripple','Stellar', 'Hyperledger Fabric','Hyperledger Sawtooth','Quorum','Corda','EOS','NEO','Waves','Steemit','Lisk','Quantum','Tezos','Cardano','Litecoin','Monero','ZCash','IOTA','NEM','NXT','Dash','Doge'];
 
 module.exports.currencies = ['� EUR', '$ USD', '� GBP'];
 
