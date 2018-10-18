dbc54aba2098488de794d50ca5b4294e75d084b2
Sadia Abbas <sadia.abbas@mwanmobile.com>
2018-10-18 12:36:47 +0500

    change in header component

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -13,9 +13,10 @@ import { DataService } from "../data.service";
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  
+
   currentUser: User;
-  user_type;is_admin;
+  user_type;
+  is_admin;
   route;
   admin_route;
   is_verify;
@@ -24,54 +25,32 @@ export class HeaderComponent implements OnInit {
   increment;
   user_name = 'Admin';
   setting;
-  log;success;
-    success_msg;
+  log;
+  success;
+  success_msg;
+  location;
 
-  constructor(private authenticationService: UserService,private dataservice: DataService,private router: Router,location: Location,private datePipe: DatePipe) 
+  constructor(private authenticationService: UserService,private dataservice: DataService,private router: Router,location: Location,private datePipe: DatePipe)
   {
-      this.success_msg='';
-      router.events.subscribe((val) => {
+    this.success_msg='';
+    router.events.subscribe((val) => {
       if(location.path() != ''){
         this.route = location.path();
-          let loc= this.route;
-         let x = loc.split("-");
-          this.admin_route = x[0];
-       
-      }
-      else {
+        let loc= this.route;
+        this.location = loc;
+        let x = loc.split("-");
+        this.admin_route = x[0];
+
+      } else {
+        //this.route = 'Home'
       }
     });
-     
-              
-   
-
-    this.currentUser = JSON.parse(localStorage.getItem('currentUser')); 
-     // //console.log(this.currentUser);
-     this.verifyEmailBar();
-
-  }
 
 
-  ngOnInit() 
-  {
-      this.success='';
-      this.success_msg = '';
-      this.msg='';
-      
-      if(this.currentUser)
-      {
-          this.dataservice.currentMessage.subscribe(message => this.msg = message);
-          setInterval(() => {  
-                                this.msg = "" ;
-                        }, 30000);
-          this.close = JSON.parse(localStorage.getItem('close_notify')); 
-       }
 
-      
-  }
 
-  verifyEmailBar()
-  {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
     if(this.currentUser )
     {
       this.user_type = this.currentUser.type;
@@ -87,7 +66,7 @@ export class HeaderComponent implements OnInit {
               if(data)
               {
                 this.is_verify = data._creator.is_verify;
-                if(this.is_verify === 0 && data.terms === true && data.description && data.locations && data.roles && data.interest_area && data.expected_salary && data.availability_day &&data.current_salary && data.current_currency &&data.current_salary && data.current_currency && data.why_work)
+                if(this.is_verify == 0)
                 {
                   this.success_msg = "not verify";
                 }
@@ -110,6 +89,7 @@ export class HeaderComponent implements OnInit {
       }
       else if(this.user_type === 'company')
       {
+
         this.authenticationService.getCurrentCompany(this.currentUser._creator)
           .subscribe(
             data =>
@@ -129,6 +109,7 @@ export class HeaderComponent implements OnInit {
                 this.user_name = data.first_name+' '+data.last_name;
                 if(this.is_admin === 1)
                 {
+                  //this.admin_route = '/admin';
                 }
                 else
                 {
@@ -145,93 +126,111 @@ export class HeaderComponent implements OnInit {
 
     }
 
-
   }
 
+  ngOnInit()
+  {
+    this.success='';
+    this.success_msg = '';
+    this.msg='';
+
+    if(this.currentUser)
+    {
+      this.dataservice.currentMessage.subscribe(message => this.msg = message);
+      setInterval(() => {
+        this.msg = "" ;
+      }, 30000);
+      this.close = JSON.parse(localStorage.getItem('close_notify'));
+    }
+
+  }
 
 
   verify_client()
   {
-      this.success_msg='';
-      this.msg='';
-      localStorage.setItem('close_notify', JSON.stringify(this.close));
-      if(this.currentUser.email)
-      {
-          console.log(this.currentUser.email);
-           this.authenticationService.verify_client(this.currentUser.email)
-            .subscribe(
-                data => {      
-                    console.log(data);
-                    if(data['msg'])
-                    {
-                        
-                        this.success_msg = "Please check your email to verify your account" ;
-                        
-                        setInterval(() => {  
-                                this.success_msg = "not verify" ;
-                                
-                        }, 12000);
-                        
-                    }
+    this.success_msg='';
+    this.msg='';
 
-                    else
-                    {
-                        this.dataservice.changeMessage(data['error']);
-                        this.log= data['error'];
-                        
-                        
-                    }
-               
-                },
-                error => {
-                 this.dataservice.changeMessage(error);
-                   
-                });
-          
-      }
-      else
-      {
-          
-      }
+    localStorage.setItem('close_notify', JSON.stringify(this.close));
+    if(this.currentUser.email)
+    {
+      console.log(this.currentUser.email);
+      this.authenticationService.verify_client(this.currentUser.email)
+        .subscribe(
+          data => {
+            console.log(data);
+            if(data['msg'])
+            {
+
+              this.success_msg = "Please check your email to verify your account" ;
+
+              setInterval(() => {
+                this.success_msg = "not verify" ;
+
+              }, 12000);
+
+            }
+
+            else
+            {
+              this.dataservice.changeMessage(data['error']);
+              this.log= data['error'];
+
+
+            }
+
+          },
+          error => {
+            this.dataservice.changeMessage(error);
+
+          });
+
+    }
+    else
+    {
+
+    }
   }
-    close;
-    close_notify()
-    {
-        this.success_msg='';
-        this.close = "close"; 
-        localStorage.setItem('close_notify', JSON.stringify(this.close)); 
-         
-    }
-    
-    logout()
-    {
-   
-         this.authenticationService.destroyToken(this.currentUser._id)
-            .subscribe(
-                data => {                    
-                    if(data)
-                    {
-                        
-                    }
-                    
-                    },
-                error =>
-                {
-                    if(error.message === 500 || error.message === 401)
-                    {
-                        localStorage.setItem('jwt_not_found', 'Jwt token not found');
-                        window.location.href = '/login';
-                    }
-                    
-                    });
+  close;
+  close_notify()
+  {
+    this.success_msg='';
+    this.close = "close";
+    localStorage.setItem('close_notify', JSON.stringify(this.close));
 
-         localStorage.removeItem('currentUser');
-         localStorage.removeItem('googleUser');
-         localStorage.removeItem('close_notify');
-         localStorage.removeItem('linkedinUser');
-         localStorage.removeItem('admin_log');
-       
-    }
-    
+  }
+
+  logout()
+  {
+
+    this.authenticationService.destroyToken(this.currentUser._id)
+      .subscribe(
+        data => {
+          if(data)
+          {
+
+          }
+
+        },
+        error =>
+        {
+          if(error.message === 500 || error.message === 401)
+          {
+            localStorage.setItem('jwt_not_found', 'Jwt token not found');
+            window.location.href = '/login';
+          }
+
+        });
+
+
+    localStorage.removeItem('currentUser');
+    localStorage.removeItem('googleUser');
+    localStorage.removeItem('close_notify');
+    localStorage.removeItem('linkedinUser');
+    localStorage.removeItem('admin_log');
+
+
+  }
+
 
 }
