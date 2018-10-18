ac3eb4d133324aaaf9de6316c75cb4339041c95d
Tayyab Hussain <tayyab.hussain@mwanmobile.com>
2018-10-18 17:39:52 +0500

    fixed issued mentioned by Sadia

--- server/test/api/chat/chatHelpers.js
+++ server/test/api/chat/chatHelpers.js
@@ -103,23 +103,6 @@ const getUserMessages = module.exports.getUserMessages = async function getUserM
 
 const insertChatFile= module.exports.insertChatFile = async function insertChatFile(receverId,msgData,chatFile,jwtToken) {
     const myFile = fs.readFileSync(chatFile.path);
-    const data = {
-        'receiver_id': receverId,
-        'sender_name': msgData.sender_name,
-        'receiver_name': msgData.receiver_name,
-        'message': chatFile.message,
-        'description': msgData.description,
-        'job_title': msgData.job_title,
-        'salary': msgData.salary,
-        'currency': msgData.currency,
-        'date_of_joining': msgData.date_of_joining,
-        'job_type': msgData.job_type,
-        'msg_tag': msgData.msg_tag,
-        'is_company_reply': msgData.is_company_reply,
-        'interview_location': msgData.interview_location,
-        'interview_time': msgData.interview_time,
-        'file_name': chatFile.name
-    };
     const res = await chai.request(server)
         .post('/users/insert_chat_file')
         .set('Authorization', jwtToken)
