b4c344bdc6c063ac6e07c261ae44b83f2b63cc4b
Tayyab Hussain <tayyab.hussain@mwanmobile.com>
2018-10-24 13:06:42 +0500

    fixed

--- server/controller/api/chat/getLastJobDescription.controller.js
+++ server/controller/api/chat/getLastJobDescription.controller.js
@@ -5,7 +5,6 @@ module.exports = async function (req, res) {
 
     const chatDoc = await chat.findOne({
         $and:[{sender_id:userId},{msg_tag: 'job_offer'}]
-    },{sender_id:1,job_title:1,salary:1,salary_currency:1,job_type:1,interview_location:1,description:1})
-    .sort({date_created: 'descending'}).lean();
+    }).sort({date_created: 'descending'}).lean();
     res.send(chatDoc);
 };
