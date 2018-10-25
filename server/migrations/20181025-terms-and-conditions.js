9833eca7c01226a81b77c8263445bbe5d2c5fa3a
Jack Tanner <jack@jackandtheblockstalk.com>
2018-10-25 12:29:05 +0100

    completed cadidate up

--- server/migrations/20181025-terms-and-conditions.js
+++ server/migrations/20181025-terms-and-conditions.js
@@ -1,24 +1,78 @@
 const Candidate = require('../model/candidate_profile');
+const Company = require('../model/employer_profile');
 const Pages = require('../model/pages_content');
 
+let totalDocsToProcess, totalProcessed = 0, totalModified = 0;
+
 // This function will perform the migration
-module.exports.up = async function up() {
+module.exports.up = async function() {
     const latestCandidateTermsPage = await Pages.findOne({page_name: 'Terms and Condition for candidate'}).lean();
     const candidateTermsPageId = latestCandidateTermsPage._id;
 
     let candidateCursor = await Candidate.find({}).cursor();
-    for (let candidateDoc = await candidateCursor.next(); candidateDoc !== null; candidateDoc = await candidateCursor.next()) {
+    totalDocsToProcess = await Candidate.find({}).count();
+    let candidateDoc = await candidateCursor.next();
+    let x = 0;
+    for ( null ; candidateDoc !== null; candidateDoc = await candidateCursor.next()) {
+        totalProcessed++;
+        candidateDoc = candidateDoc.toObject();
+// if(x === 0) console.log(candidateDoc);
         console.log("Updating " + candidateDoc._id);
-        console.log("  candidate.terms " + candidateDoc.terms);
         if (candidateDoc.terms) {
+            console.log('  Setting terms_id = ' + candidateTermsPageId);
             candidateDoc.terms_id = candidateTermsPageId;
         }
         delete candidateDoc.terms;
-        candidateDoc.save();
+        if (!candidateDoc.github_account) {
+            console.log('removing github_account field');
+            console.log(delete candidateDoc.github_account);
+        }
+        if (!candidateDoc.stackexchange_account) {
+            console.log('removing stackexchange_account field');
+            delete candidateDoc.stackexchange_account;
+        }
+        // if(x === 0) console.log(candidateDoc);
+
+        const update = await Candidate.update({_id: candidateDoc._id}, candidateDoc);
+        console.log(update);
+        if (update) totalModified++;
+        x=1;
     }
+
+    console.log('Total candidates docs to process: ', totalDocsToProcess);
+    console.log('Total processed: ', totalProcessed);
+    console.log('Total modified: ', totalModified);
+
+    totalProcessed = 0;
+    totalModified = 0;
+
+    // const latestCompanyTermsPage = await Pages.findOne({page_name: 'Terms and Condition for company'}).lean();
+    // const companyTermsPageId = latestCompanyTermsPage._id;
+    //
+    // let companyCursor = await Candidate.find({}).cursor();
+    // totalDocsToProcess = await Candidate.find({}).count();
+    // let companyDoc = await companyCursor.next();
+    // for ( null ; companyDoc !== null; companyDoc = await companyCursor.next()) {
+    //     totalProcessed++;
+    //     companyDoc = companyDoc.toObject();
+    //
+    //     console.log("Updating " + companyDoc._id);
+    //     if (companyDoc.terms) {
+    //         console.log('  Setting terms_id = ' + companyTermsPageId);
+    //         companyDoc.terms_id = companyTermsPageId;
+    //     }
+    //     delete companyDoc.terms;
+    //
+    //     await Company.update({_id: companyDoc._id}, companyDoc);
+    //     totalModified++;
+    // }
+    //
+    // console.log('Total company docs to process: ', totalDocsToProcess);
+    // console.log('Total processed: ', totalProcessed);
+    // console.log('Total modified: ', totalModified);
 }
 
 // This function will undo the migration
-module.exports.down = async function down() {
+module.exports.down = async function() {
     console.log('Down')
 }
