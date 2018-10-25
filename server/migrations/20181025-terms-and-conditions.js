b75acbabf2933bedce823eeba37a37b7d43abf6e
Jack Tanner <jack@jackandtheblockstalk.com>
2018-10-25 16:06:57 +0100

    finished up and down

--- server/migrations/20181025-terms-and-conditions.js
+++ server/migrations/20181025-terms-and-conditions.js
@@ -4,6 +4,16 @@ const Pages = require('../model/pages_content');
 
 let totalDocsToProcess, totalProcessed = 0, totalModified = 0;
 
+
+function isEmptyObject(obj) {
+    for(let prop in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+            return false;
+        }
+    }
+    return true;
+}
+
 // This function will perform the migration
 module.exports.up = async function() {
     const latestCandidateTermsPage = await Pages.findOne({page_name: 'Terms and Condition for candidate'}).lean();
@@ -12,31 +22,40 @@ module.exports.up = async function() {
     let candidateCursor = await Candidate.find({}).cursor();
     totalDocsToProcess = await Candidate.find({}).count();
     let candidateDoc = await candidateCursor.next();
-    let x = 0;
+
     for ( null ; candidateDoc !== null; candidateDoc = await candidateCursor.next()) {
         totalProcessed++;
-        candidateDoc = candidateDoc.toObject();
-// if(x === 0) console.log(candidateDoc);
+
+        let unset = {};
+        let set = {};
         console.log("Updating " + candidateDoc._id);
         if (candidateDoc.terms) {
-            console.log('  Setting terms_id = ' + candidateTermsPageId);
-            candidateDoc.terms_id = candidateTermsPageId;
+            set.terms_id = candidateTermsPageId;
+            unset.terms = 1;
         }
-        delete candidateDoc.terms;
-        if (!candidateDoc.github_account) {
-            console.log('removing github_account field');
-            console.log(delete candidateDoc.github_account);
-        }
-        if (!candidateDoc.stackexchange_account) {
-            console.log('removing stackexchange_account field');
-            delete candidateDoc.stackexchange_account;
-        }
-        // if(x === 0) console.log(candidateDoc);
 
-        const update = await Candidate.update({_id: candidateDoc._id}, candidateDoc);
-        console.log(update);
-        if (update) totalModified++;
-        x=1;
+        if (candidateDoc.github_account === null) {
+            unset.github_account = 1
+        }
+        if (candidateDoc.stackexchange_account === null) {
+            unset.stackexchange_account = 1;
+        }
+
+        let updateObj;
+        if (!isEmptyObject(set) && !isEmptyObject(unset)) {
+            updateObj = {$set: set, $unset: unset}
+        } else if (!isEmptyObject(set)) {
+            updateObj = {$set: set};
+        } else if (!isEmptyObject(unset)) {
+            updateObj = {$unset: unset};
+        }
+
+        if (updateObj) {
+            console.log('  ', updateObj);
+            const update = await Candidate.update({_id: candidateDoc._id}, updateObj);
+            if (update && update.nModified) totalModified++;
+            else console.log('  UPDATE NOT SUCESSFUL');
+        }
     }
 
     console.log('Total candidates docs to process: ', totalDocsToProcess);
@@ -46,33 +65,85 @@ module.exports.up = async function() {
     totalProcessed = 0;
     totalModified = 0;
 
-    // const latestCompanyTermsPage = await Pages.findOne({page_name: 'Terms and Condition for company'}).lean();
-    // const companyTermsPageId = latestCompanyTermsPage._id;
-    //
-    // let companyCursor = await Candidate.find({}).cursor();
-    // totalDocsToProcess = await Candidate.find({}).count();
-    // let companyDoc = await companyCursor.next();
-    // for ( null ; companyDoc !== null; companyDoc = await companyCursor.next()) {
-    //     totalProcessed++;
-    //     companyDoc = companyDoc.toObject();
-    //
-    //     console.log("Updating " + companyDoc._id);
-    //     if (companyDoc.terms) {
-    //         console.log('  Setting terms_id = ' + companyTermsPageId);
-    //         companyDoc.terms_id = companyTermsPageId;
-    //     }
-    //     delete companyDoc.terms;
-    //
-    //     await Company.update({_id: companyDoc._id}, companyDoc);
-    //     totalModified++;
-    // }
-    //
-    // console.log('Total company docs to process: ', totalDocsToProcess);
-    // console.log('Total processed: ', totalProcessed);
-    // console.log('Total modified: ', totalModified);
+    const latestCompanyTermsPage = await Pages.findOne({page_name: 'Terms and Condition for company'}).lean();
+    const companyTermsPageId = latestCompanyTermsPage._id;
+
+    let companyCursor = await Company.find({}).cursor();
+    totalDocsToProcess = await Company.find({}).count();
+    let companyDoc = await companyCursor.next();
+    for ( null ; companyDoc !== null; companyDoc = await companyCursor.next()) {
+        totalProcessed++;
+
+        console.log("Updating " + companyDoc._id);
+        if (companyDoc.terms) {
+            const updateObj = {
+                $set: {terms_id: companyTermsPageId},
+                $unset: {terms: 1}
+            };
+            console.log('  ', updateObj);
+            const update = await Company.update({_id: companyDoc._id}, updateObj);
+            if (update && update.nModified) totalModified++;
+            else console.log('  UPDATE NOT SUCESSFUL');
+        }
+    }
+
+    console.log('Total company docs to process: ', totalDocsToProcess);
+    console.log('Total processed: ', totalProcessed);
+    console.log('Total modified: ', totalModified);
 }
 
 // This function will undo the migration
 module.exports.down = async function() {
-    console.log('Down')
+    const latestCandidateTermsPage = await Pages.findOne({page_name: 'Terms and Condition for candidate'}).lean();
+    const candidateTermsPageId = latestCandidateTermsPage._id;
+
+    let candidateCursor = await Candidate.find({}).cursor();
+    totalDocsToProcess = await Candidate.find({}).count();
+    let candidateDoc = await candidateCursor.next();
+
+    for ( null ; candidateDoc !== null; candidateDoc = await candidateCursor.next()) {
+        totalProcessed++;
+
+        console.log("Updating " + candidateDoc._id);
+        if (candidateDoc.terms_id) {
+            const updateObj = {
+                $unset: {terms_id: 1},
+                $set: {terms: true}
+            };
+            console.log('  ', updateObj);
+            const update = await Candidate.update({_id: candidateDoc._id}, updateObj);
+            if (update && update.nModified) totalModified++;
+            else console.log('  UPDATE NOT SUCESSFUL');
+        }
+    }
+
+    console.log('Total candidates docs to process: ', totalDocsToProcess);
+    console.log('Total processed: ', totalProcessed);
+    console.log('Total modified: ', totalModified);
+
+    totalProcessed = 0;
+    totalModified = 0;
+
+    let companyCursor = await Company.find({}).cursor();
+    totalDocsToProcess = await Company.find({}).count();
+    let companyDoc = await companyCursor.next();
+    for ( null ; companyDoc !== null; companyDoc = await companyCursor.next()) {
+        totalProcessed++;
+
+        console.log("Updating " + companyDoc._id);
+        if (companyDoc.terms_id) {
+            const updateObj = {
+                $unset: {terms_id: 1},
+                $set: {terms: true}
+            };
+            console.log('  ', updateObj);
+            const update = await Company.update({_id: companyDoc._id}, updateObj);
+            if (update && update.nModified) totalModified++;
+            else console.log('  UPDATE NOT SUCESSFUL');
+        }
+    }
+
+    console.log('Total company docs to process: ', totalDocsToProcess);
+    console.log('Total processed: ', totalProcessed);
+    console.log('Total modified: ', totalModified);
 }
