8782a504bacab7ea774593ddada238cee95892c6
Jack Tanner <jack@jackandtheblockstalk.com>
2018-10-31 10:52:59 +0000

    fixed migration script

--- server/model/candidate_profile.js
+++ server/model/candidate_profile.js
@@ -5,7 +5,10 @@ const enumerations = require('./enumerations');
 const Schema = mongoose.Schema;
 
 const CandidateProfileSchema = new Schema({
-	terms_id:
+	terms: {
+	    type: Boolean
+    },
+    terms_id:
     {
         type: Schema.Types.ObjectId,
         ref: 'pages_content'
