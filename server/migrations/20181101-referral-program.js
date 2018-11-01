e0a448fc4682687a63d77986fc3d39fb038e618a
Sadia Abbas <sadia.abbas@mwanmobile.com>
2018-11-01 20:02:11 +0500

    some updation in referral

--- server/migrations/20181101-referral-program.js
+++ server/migrations/20181101-referral-program.js
@@ -6,30 +6,31 @@ let totalDocsToProcess, totalProcessed = 0, totalModified = 0;
 
 // This function will perform the migration
 module.exports.up = async function() {
-    const userCursor = await Users.find({}).lean();
-    totalDocsToProcess = await Users.find({}).count();
+    const userCursor = await Users.find().cursor();
+    totalDocsToProcess = await Users.find({type : 'candidate'}).count();
+    console.log(totalDocsToProcess);
     let userDoc = await userCursor.next();
 
     for ( null ; userDoc !== null; userDoc = await userCursor.next()) {
-        totalProcessed++;
 
         let unset = {};
-        if (userDoc.ref_link && userDoc.email) {
+        if (userDoc.ref_link && userDoc.email && userDoc.type === 'candidate') {
+            totalProcessed++;
+
             let referralToken = userDoc.ref_link.substr(userDoc.ref_link.length - 10);
             let document = new Referral
             ({
-                _id :  userDoc.referred_id,
+                _id :  userDoc.refered_id,
                 email : userDoc.email,
                 url_token : referralToken,
                 date_created: new Date(),
             });
             const refObj = await document.save();
-            console.log(refObj);
             unset.ref_link = 1;
             let updateObj = {$unset : unset}
 
             if (updateObj) {
-                console.log('  ', updateObj);
+                //console.log('  ', updateObj);
                 const update = await Users.update({_id: userDoc._id}, updateObj);
                 if (update && update.nModified) totalModified++;
                 else console.log('  UPDATE NOT SUCESSFUL');
@@ -52,9 +53,10 @@ module.exports.down = async function() {
 
 
     for ( null ; referralDoc !== null; referralDoc = await referralCursor.next()) {
-        totalProcessed++;
 
         if (referralDoc.url_token && referralDoc.email === userDoc.email) {
+            totalProcessed++;
+
             const updateObj = {
                 $set: {refered_id: referralDoc._id , ref_link : referralDoc.url_token}
             };
