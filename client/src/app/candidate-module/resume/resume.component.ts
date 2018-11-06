d6ffb684e67480d0fe77c10cd2000c043edb0416
Sadia Abbas <sadia.abbas@mwanmobile.com>
2018-11-06 12:08:20 +0500

    working in resume FE

--- client/src/app/candidate-module/resume/resume.component.ts
+++ client/src/app/candidate-module/resume/resume.component.ts
@@ -29,6 +29,9 @@ export class ResumeComponent implements OnInit,AfterViewInit {
   term_active_class;term_link;
     exp_disable;
   error_msg;
+  skill_expYear_db=[];
+  skillDbArray=[];
+  skillDb;
 
    constructor(private route: ActivatedRoute, private http: HttpClient,
         private router: Router,
@@ -76,7 +79,7 @@ export class ResumeComponent implements OnInit,AfterViewInit {
                       this.term_active_class='fa fa-check-circle text-success';
                      this.term_link = '/terms-and-condition';
                   }
-               //////console.log(data);
+               console.log(data);
                 if(data.commercial_platform || data.experimented_platform || data.why_work || data.platforms)
                 {
                   this.why_work=data.why_work;
@@ -192,52 +195,53 @@ export class ResumeComponent implements OnInit,AfterViewInit {
                       }
 
                     }
-                    // this.expYear = data.experimented_platform;
-                    /* for (let key of data.experimented_platform)
-                      {
-                        for(var i in key)
-                        {
 
-
-                          for(let option of this.experimented)
-                          {
-
-                            if(option.value == key[i])
-                            {
-                              option.checked=true;
-                              this.exp_db_valye.push(key[i]);
-                              this.exp_db_lang= ({value: key[i]});
-                              this.experimented_platform.push(this.exp_db_lang);
-
-                            }
-                            else
-                            {
-
-                            }
-
-                          }
-
-
-                          for(let option of this.exp_year)
-                          {
-
-                            if(option.value == key[i])
-                            {
-                                option.checked=true;
-
-                                //this.expYear.push(option);
-                                this.exp_expYear.push(key[i]);
-
-                            }
-
-                          }
-
-                        }
-                      }*/
                     }
 
 
                 }
+
+                  if(data._creator.candidate.blockchain && data._creator.candidate.blockchain.commercial_skills)
+                  {
+                    this.commercialSkillsExperienceYear = data._creator.candidate.blockchain.commercial_skills;
+                    for (let key of data._creator.candidate.blockchain.commercial_skills)
+                    {
+                      for(var i in key)
+                      {
+
+                        for(let option of this.otherSkills)
+                        {
+
+                          if(option.value === key[i])
+                          {
+                            option.checked=true;
+                            this.skillDbArray.push(key[i]);
+                            this.skillDb= ({value: key[i]});
+                            this.commercialSkills.push(this.skillDb);
+
+                          }
+                          else
+                          {
+
+                          }
+
+                        }
+
+                        for(let option of this.exp_year)
+                        {
+
+                          if(option.value === key[i])
+                          {
+                            option.checked=true;
+                            this.skill_expYear_db.push(key[i]);
+
+                          }
+
+                        }
+
+                      }
+                    }
+                  }
 
                 if(data.locations && data.roles && data.interest_area || data.expected_salary || data.availability_day && data.current_salary )
                   {
@@ -320,6 +324,19 @@ export class ResumeComponent implements OnInit,AfterViewInit {
 
   ]
 
+  otherSkills =
+    [
+      {name:'P2P protocols', value:'P2P protocols', checked:false},
+      {name:'Distributed computing and networks', value:'Distributed computing and networks', checked:false},
+      {name:'Security', value:'Security', checked:false},
+      {name:'Formal verification', value:'Formal verification', checked:false},
+      {name:'Cryptography', value:'Cryptography', checked:false},
+      {name:'Game theory', value:'Game theory', checked:false},
+      {name:'Economics', value:'Economics', checked:false},
+      {name:'Smart contract audits', value:'Smart contract audits', checked:false},
+      {name:'Zero Knowlege Proofs', value:'Zero Knowlege Proofs', checked:false},
+    ]
+
   designed=
   [
      {name:'Bitcoin', value:'Bitcoin', checked:false},
@@ -391,27 +408,7 @@ export class ResumeComponent implements OnInit,AfterViewInit {
       }
 
 
-      /* let updateItem = this.experimented_platform.find(this.findIndexToUpdate, obj.value);
-      let index = this.experimented_platform.indexOf(updateItem);
-      if(index > -1)
-      {
-        this.experimented_platform.splice(index, 1);
-          let updateItem2 = this.findObjectByKey(this.expYear, 'experimented_platform', obj.value);
-       //////console.log(updateItem);
-      let index2 = this.expYear.indexOf(updateItem2);
 
-      if(index2 > -1)
-      {
-
-        this.expYear.splice(index2, 1);
-          }
-      }
-      else
-      {
-        obj.checked =true;
-        this.experimented_platform.push(obj);
-      }
-      */
   }
 
 
@@ -421,6 +418,7 @@ export class ResumeComponent implements OnInit,AfterViewInit {
     }
 
     why_work_log;commercial_log;platform_log;
+  commercial_skill_log;
    blockchain_exp(expForm: NgForm)
    {
      this.error_msg="";
@@ -432,6 +430,10 @@ export class ResumeComponent implements OnInit,AfterViewInit {
         {
             this.commercial_log = "Please fill year of experience";
         }
+     if(this.commercialSkills.length !== this.commercialSkillsExperienceYear.length)
+     {
+       this.commercial_skill_log = "Please fill year of experience";
+     }
         if(this.platforms_designed.length !== this.platforms.length)
         {
             this.platform_log = "Please fill year of experience";
@@ -441,9 +443,10 @@ export class ResumeComponent implements OnInit,AfterViewInit {
         {
           this.why_work_log = "Please fill why do you want to work on blockchain?";
          }
-       if(this.why_work && this.commercially_worked.length === this.commercial_expYear.length && this.platforms_designed.length === this.platforms.length)
+       if(this.why_work && this.commercially_worked.length === this.commercial_expYear.length && this.platforms_designed.length === this.platforms.length
+       && this.platforms_designed.length === this.platforms.length)
        {
-        this.authenticationService.resume(this.currentUser._creator,expForm.value)
+        this.authenticationService.resume(this.currentUser._creator , expForm.value)
             .subscribe(
                 data => {
                 if(data && this.currentUser)
@@ -642,21 +645,7 @@ export class ResumeComponent implements OnInit,AfterViewInit {
 
    }
 
-   /*onPlatformOptions(e)
-   {
-      if(e.target.checked)
-    {
-        this.platform.push(e.target.value);
-    }
 
-    else
-    {
-      let updateItem = this.platform.find(this.findIndexToUpdate, e.target.value.maintenancetype);
-      let index = this.platform.indexOf(updateItem);
-      this.platform.splice(index, 1);
-    }
-    //////console.log(this.platform);
-   }*/
 
    findObjectByKey(array, key, value)
     {
@@ -673,5 +662,62 @@ export class ResumeComponent implements OnInit,AfterViewInit {
           }
         return null;
     }
+
+  commercialSkills=[];
+  commercialSkillsExperienceYear=[];
+
+  oncommercialSkillsOptions(obj)
+  {
+
+    let updateItem = this.commercialSkills.find(this.findIndexToUpdate, obj.value);
+    let index = this.commercialSkills.indexOf(updateItem);
+    if(index > -1)
+    {
+      this.commercialSkills.splice(index, 1);
+      let updateItem2 = this.findObjectByKey(this.commercialSkillsExperienceYear, 'platform_name',  obj.value);
+      let index2 = this.commercialSkillsExperienceYear.indexOf(updateItem2);
+
+      if(index2 > -1)
+      {
+
+        this.commercialSkillsExperienceYear.splice(index2, 1);
+      }
+    }
+    else
+    {
+      obj.checked =true;
+      this.commercialSkills.push(obj);
+    }
+    console.log(this.commercialSkills);
+
+  }
+
+  onComSkillExpYearOptions(e, value)
+  {
+    this.selectedValue = e.target.value;
+    let updateItem = this.findObjectByKey(this.commercialSkillsExperienceYear, 'skill', value);
+    let index = this.commercialSkillsExperienceYear.indexOf(updateItem);
+
+    if(index > -1)
+    {
+
+      this.commercialSkillsExperienceYear.splice(index, 1);
+      this.value = value;
+      this.referringData = { skill : this.value, exp_year: e.target.value};
+      this.commercialSkillsExperienceYear.push(this.referringData);
+      console.log(this.commercialSkillsExperienceYear);
+
+    }
+    else
+    {
+      this.value=value;
+      this.referringData = { skill : this.value, exp_year: e.target.value};
+      this.commercialSkillsExperienceYear.push(this.referringData);
+      console.log(this.commercialSkillsExperienceYear);
+
+    }
+
+  }
+
 
 }
