c511bff88195770df430f2b28e0e933b83f21d97
Sadia Abbas <sadia.abbas@mwanmobile.com>
2018-11-07 17:57:39 +0500

    change in app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component , AfterViewInit} from '@angular/core';
+import {
+  Router, NavigationStart, NavigationCancel, NavigationEnd
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements AfterViewInit {
+  loading;
   title = 'app';
      message: any;
-  constructor() {
-    
+  constructor(private router: Router) {
+    this.loading = true;
    }
+  ngAfterViewInit() {
+    this.router.events
+      .subscribe((event) => {
+        if(event instanceof NavigationStart) {
+          this.loading = true;
+        }
+        else if (
+          event instanceof NavigationEnd ||
+          event instanceof NavigationCancel
+        ) {
+          this.loading = false;
+        }
+      });
+  }
 
 }
