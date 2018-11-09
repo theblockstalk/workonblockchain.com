edb2d048b1e2f0005dbcc08c8164e425daaaa30a
Sadia Abbas <sadia.abbas@mwanmobile.com>
2018-11-09 11:29:09 +0500

    convert getCompany to async

--- server/controller/api/users/company/getCompany.controller.js
+++ server/controller/api/users/company/getCompany.controller.js
@@ -1,51 +1,19 @@
-const settings = require('../../../../settings');
-var Q = require('q');
 const EmployerProfile = require('../../../../model/employer_profile');
-const logger = require('../../../services/logger');
 const filterReturnData = require('../filterReturnData');
 
-//////////get sign-up data from db of all companies////////////
+module.exports = async function (req, res) {
 
-module.exports = function (req, res)
-{
-    getCompany().then(function (users)
+    const employerDoc = await EmployerProfile.find().populate('_creator').lean();
+
+    var employerArray=[];
+    employerDoc.forEach(function(detail)
     {
-        res.send(users);
-    })
-        .catch(function (err)
+        if(detail._creator != null)
         {
-            res.status(400).send(err);
-        });
-}
-
-function getCompany()
-{
-    var deferred = Q.defer();
-
-    EmployerProfile.find().populate('_creator').exec(function(err, result)
-    {
-        if (err)
-        {
-            logger.error(err.message, {stack: err.stack});
-            deferred.reject(err.name + ': ' + err.message);
+            employerArray.push(filterReturnData.removeSensativeData(detail));
         }
-        else
-        {
-        	var array=[];
-       	 	result.forEach(function(item)
-            {
-       	 		if(item._creator != null)
-       	 		{
-       	 		   array.push(filterReturnData.removeSensativeData(item.toObject()));
-       	 		}
-                   
-            });
-            deferred.resolve(array);
-        	
-        }
-               
-      
+
     });
 
-    return deferred.promise;
+    res.send(employerArray);
 }
--- server/routes.js
+++ server/routes.js
@@ -110,7 +110,7 @@ router.put('/users/update_profile', auth.isLoggedIn, asyncMiddleware(candidateUp
 
 // Companies
 router.post('/users/create_employer',  asyncMiddleware(companyRegister));
-router.get('/users/company',auth.isAdmin, companyGet);
+router.get('/users/company',auth.isAdmin, asyncMiddleware(companyGet));
 router.get('/users/current_company/:_id',auth.isLoggedIn, companyGetCurrent);
 router.put('/users/company_wizard',auth.isLoggedIn, companyWizardTnT);
 router.put('/users/about_company',auth.isLoggedIn, companyWizardAbout);
