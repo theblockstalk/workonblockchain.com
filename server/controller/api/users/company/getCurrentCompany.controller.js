0660cdf64c004cc4562cded9d97c3accd70cdaa5
Sadia Abbas <sadia.abbas@mwanmobile.com>
2018-11-14 16:06:48 +0500

    some fixes

--- server/controller/api/users/company/getCurrentCompany.controller.js
+++ server/controller/api/users/company/getCurrentCompany.controller.js
@@ -4,23 +4,20 @@ const errors = require('../../../services/errors');
 
 module.exports = async function (req, res) {
 
-    let userId = req.auth.user;
-    if(userId._id === req.params._id || userId.is_admin === 1 ) {
-        let employerProfile = await EmployerProfile.findById(req.params._id).populate('_creator').lean();
-        if(employerProfile){
-            const employerRes =  filterReturnData.removeSensativeData(employerProfile);
-            res.send(employerRes);
+    let employerProfile = await EmployerProfile.findById(req.params._id).populate('_creator').lean();
+    if(employerProfile){
+        const employerRes =  filterReturnData.removeSensativeData(employerProfile);
+        res.send(employerRes);
+    }
+    else {
+        employerProfile =  await EmployerProfile.find({_creator : req.params._id}).populate('_creator').lean();
+        if(employerProfile && employerProfile.length > 0){
+            const employerCreatorRes = filterReturnData.removeSensativeData(employerProfile[0]);
+            res.send(employerCreatorRes);
         }
-        else {
-            employerProfile =  await EmployerProfile.find({_creator : req.params._id}).populate('_creator').lean();
-            if(employerProfile && employerProfile.length > 0){
-                const employerCreatorRes = filterReturnData.removeSensativeData(employerProfile[0]);
-                res.send(employerCreatorRes);
-            }
-            else
-            {
-                errors.throwError("User not found", 404)
-            }
+        else
+        {
+            errors.throwError("User not found", 404)
         }
     }
 
