79431986765cbd59c1b0bb98749e8d8f42021fcf
Sadia Abbas <sadia.abbas@mwanmobile.com>
2018-11-14 16:41:34 +0500

    some fixes in linkedin import

--- client/src/app/candidate-module/linkedin-import/linkedin-import.component.ts
+++ client/src/app/candidate-module/linkedin-import/linkedin-import.component.ts
@@ -414,18 +414,7 @@ export class LinkedinImportComponent implements OnInit {
                     }
                   },
                   error => {
-                    if(error.message === 500 || error.message === 401)
-                    {
-                      localStorage.setItem('jwt_not_found', 'Jwt token not found');
-                      localStorage.removeItem('currentUser');
-                      localStorage.removeItem('googleUser');
-                      localStorage.removeItem('close_notify');
-                      localStorage.removeItem('linkedinUser');
-                      localStorage.removeItem('admin_log');
-                      window.location.href = '/login';
-                    }
-
-                    if(error.message === 403)
+                    if(error['status'] === 404 && error['error']['message'] && error['error']['requestID'] && error['error']['success'] === false)
                     {
                       this.router.navigate(['/not_found']);
                     }
