9b3e4f666e935aded457eca036cb3d2ba698008a
Sadia Abbas <sadia.abbas@mwanmobile.com>
2018-11-15 17:12:58 +0500

    some fixes

--- server/controller/api/users/auth/account_setting.controller.js
+++ server/controller/api/users/auth/account_setting.controller.js
@@ -8,4 +8,7 @@ module.exports = async function (req,res) {
     const queryBody = req.body;
     const timestamp = new Date();
     await User.update({ _id:userId },{ $set: {'disable_account': queryBody.status, 'dissable_account_timestamp' : timestamp } });
+    res.send({
+        success : true
+    })
 }
