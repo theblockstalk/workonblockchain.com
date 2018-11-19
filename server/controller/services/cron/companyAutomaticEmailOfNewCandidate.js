42c2af137f9343a316cb462a8ac46d4750cc76ad
Sadia Abbas <sadia.abbas@mwanmobile.com>
2018-11-19 17:33:39 +0500

    update error

--- server/controller/services/cron/companyAutomaticEmailOfNewCandidate.js
+++ server/controller/services/cron/companyAutomaticEmailOfNewCandidate.js
@@ -12,7 +12,7 @@ module.exports = async function (req, res) {
     if(userDoc && userDoc.length > 0) {
         for(let i=0; i < userDoc.length; i++) {
             let queryString = [];
-            if(userDoc[i].saved_searches[0].last_email_sent  < now - when_receive_email_notitfications) { // not sure about this
+            if(userDoc[i].saved_searches && userDoc[i].saved_searches[0].last_email_sent  < now - when_receive_email_notitfications) { // not sure about this
                 if (userDoc[i].saved_searches[0] && userDoc[i].saved_searches[0].length > 0 && userDoc[i].saved_searches[0].receive_email_notitfications === true) {
                     if (userDoc[i].saved_searches[0].location) {
                         const locationFilter = {"locations": {$in: userDoc[i].saved_searches[0].location}};
