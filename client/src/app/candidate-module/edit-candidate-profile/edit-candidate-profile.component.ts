5d72bd92e2f4cb982b70ed7a61f35edfcea78596
Sadia Abbas <sadia.abbas@mwanmobile.com>
2018-11-19 10:12:55 +0500

    added some console to find the issue

--- client/src/app/candidate-module/edit-candidate-profile/edit-candidate-profile.component.ts
+++ client/src/app/candidate-module/edit-candidate-profile/edit-candidate-profile.component.ts
@@ -1452,7 +1452,7 @@ export class EditCandidateProfileComponent implements OnInit,AfterViewInit {
       }
       this.experiencejson = {companyname : this.ExperienceForm.value.ExpItems[key].companyname , positionname : this.ExperienceForm.value.ExpItems[key].positionname,locationname : this.ExperienceForm.value.ExpItems[key].locationname,description : this.ExperienceForm.value.ExpItems[key].description,startdate : this.start_date_format,enddate : this.end_date_format , currentwork : this.ExperienceForm.value.ExpItems[key].currentwork};
       this.experiencearray.push(this.experiencejson);
-
+console.log(this.experiencearray);
     }
 
     for ( var key in this.EducationForm.value.itemRows)
