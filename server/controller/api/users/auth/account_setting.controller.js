5115db4cfcd434470fdae701fa5b6ce111d159e8
Sadia Abbas <sadia.abbas@mwanmobile.com>
2018-11-22 15:45:30 +0500

    removed commeted code

--- server/controller/api/users/auth/account_setting.controller.js
+++ server/controller/api/users/auth/account_setting.controller.js
@@ -4,15 +4,6 @@ const CandidateProfile = require('../../../../model/candidate_profile');
 const EmployerProfile = require('../../../../model/employer_profile');
 
 module.exports = async function (req,res) {
-
-    /*let userId = req.auth.user._id;
-    const queryBody = req.body;
-    const timestamp = new Date();
-    await User.update({ _id:userId },{ $set: {'disable_account': queryBody.status, 'dissable_account_timestamp' : timestamp } });
-    res.send({
-        success : true
-    })*/
-
     let user = req.auth.user;
     const queryBody = req.body.status;
     const timestamp = new Date();
