18616f0277076b96ac32071ee666b21cb11fd15a
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-01-02 10:22:45 +0500

    candidate wizards fix

--- client/src/app/candidate-module/experience/experience.component.ts
+++ client/src/app/candidate-module/experience/experience.component.ts
@@ -324,7 +324,7 @@ export class ExperienceComponent implements OnInit , AfterViewInit
     [
       "2023","2022","2021","2020","2019","2018","2017","2016","2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003","2002","2001","2000","1999","1998","1997","1996","1995","1994"
     ]
-    month= ["January","Februray","March","April","May","June","July","August","September","October","November","December"]
+    month= ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
     onExpOptions(obj)
     {
