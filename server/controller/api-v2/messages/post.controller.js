617e7a5f9e2b43bdc78160607cd5b3db88c96745
Jack Tanner <jack@jackandtheblockstalk.com>
2019-01-08 22:23:16 +0000

    cleanup

--- server/controller/api-v2/messages/post.controller.js
+++ server/controller/api-v2/messages/post.controller.js
@@ -18,7 +18,7 @@ const bodySchema = new Schema({
         required: true
     }
     // message: {
-    //
+    //     TODO
     // }
 });
 
@@ -27,8 +27,8 @@ module.exports.inputValidation = {
 };
 
 module.exports.auth = async function(req) {
-    console.log('in auth function')
-    // await auth.isValidUser(req);
+    console.log('in auth')
+    await auth.isValidUser(req);
 
     // if (req.query.admin) {
     //     await auth.isAdmin(req);
@@ -37,43 +37,8 @@ module.exports.auth = async function(req) {
 
 module.exports.endpoint = async function (req, res) {
     console.log('in endpoint')
-    // let sanitizeddescription = sanitize.sanitizeHtml(req.unsanitizedBody.description);
-    // let sanitizedmessage = sanitize.sanitizeHtml(req.unsanitizedBody.message);
-    // let new_description = '';
-    // let new_msg = '';
-    // if(sanitizeddescription) {
-    //     new_description = chatHelper.replaceLineBreaksHtml(sanitizeddescription);
-    // }
-    // if(sanitizedmessage) {
-    //     new_msg = chatHelper.replaceLineBreaksHtml(sanitizedmessage);
-    // }
-    //
-    // let userId = req.auth.user._id;
-    //
-    // let interview_date = '';
-    // if(req.body.msg_tag === 'interview_offer'){
-    //     interview_date = req.body.date_of_joining+' '+req.body.interview_time+':00';
-    // }
-    //
-    // let newChat = new chat({
-    //     sender_id : mongoose.Types.ObjectId(userId),
-    //     receiver_id : mongoose.Types.ObjectId(req.body.receiver_id),
-    //     sender_name: req.body.sender_name,
-    //     receiver_name: req.body.receiver_name,
-    //     message: new_msg,
-    //     description: new_description,
-    //     job_title: req.body.job_title,
-    //     salary: req.body.salary,
-    //     salary_currency: req.body.currency,
-    //     date_of_joining: '',
-    //     msg_tag: req.body.msg_tag,
-    //     is_company_reply: req.body.is_company_reply,
-    //     job_type: req.body.job_type,
-    //     is_read: 0,
-    //     interview_location: req.body.interview_location,
-    //     interview_date_time: interview_date,
-    //     date_created: new Date()
-    // });
-    // await newChat.save();
+
+    // TODO
+
     res.send('This is a response');
 };
--- server/routes-v2.js
+++ server/routes-v2.js
@@ -7,36 +7,37 @@ const endpoints = [
     require('./controller/api-v2/messages/post.controller')
 ];
 
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
+
 const validateInputs = function(inputSchemas) {
     console.log('in validating inputs');
     const validationTypes = ['query', 'params', 'body'];
 
-    return function (req, res, next) {
-        console.log('in schema validation')
+    return function (req) {
         for (const type of validationTypes) {
-            const schema = inputSchemas[type];
-            if (schema) {
-                const input = req[type];
-                if (input) {
-                    console.log('validating ' + type);
-                    console.log(input, schema.obj);
-                    const model = mongoose.model('modelToValidate' + Date.now().toString(), schema);
-                    const doc = new model(input);
-                    // let a = mongoose;
-                    // const doc = new mongoose.Document(input, schema);
-                    const errors = doc.validateSync();
-                    if (errors) throw new Error(JSON.stringify(errors));
-                }
+            const input = req[type];
+            if (input && !isEmpty(input)) {
+                console.log('validating ' + type, input);
+                const schema = inputSchemas[type];
+                const model = mongoose.model(type + Date.now().toString(), schema)
+                const doc = new model(input);
+                const error = doc.validateSync();
+                if (error) throw new Error(error);
             }
         }
-        next();
     }
 };
 
 const register = function(endpoint) {
     const path = '/v2' + endpoint.request.path;
     router[endpoint.request.type](path,
-        asyncMiddleware(validateInputs(endpoint.inputValidation)),
+        asyncMiddleware.thenNext(validateInputs(endpoint.inputValidation)),
         asyncMiddleware.thenNext(endpoint.auth),
         asyncMiddleware(endpoint.endpoint)
     );
