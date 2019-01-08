e63b78bd0657e12028995f948ded75296a9aedc4
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-01-08 12:05:49 +0500

    remove change in getCandidate controller

--- server/controller/api/chat/getCandidate.controller.js
+++ server/controller/api/chat/getCandidate.controller.js
@@ -1,7 +1,6 @@
 const users = require('../../../model/users');
 const CandidateProfile = require('../../../model/candidate_profile');
 const EmployerProfile = require('../../../model/employer_profile');
-const errors = require('../../services/errors');
 
 const filterReturnData = require('../users/filterReturnData');
 
