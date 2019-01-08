60c52b8fba415bb70f3e857139c404f1be510b4f
Jack Tanner <jack@jackandtheblockstalk.com>
2019-01-08 19:18:28 +0000

    new test file for new endpoint

---
+++ server/test/api-v2/messages/post.controller.spec.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const mongo = require('../../helpers/mongo');
+// const Chats = require('../../../model/chat');
+const messagesHelpers = require('./helpers');
+
+const assert = chai.assert;
+const expect = chai.expect;
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('send a message', function () {
+
+    afterEach(async () => {
+        console.log('dropping database');
+        await mongo.drop();
+    })
+
+    describe('POST /messages', () => {
+
+        it('it should send a message', async () => {
+
+            const res = await messagesHelpers.post({user_id: "heathea", msg_tag: 'job_offer'}, 'fake jwt token');
+            res.status.should.equal(200);
+         })
+    })
+});
