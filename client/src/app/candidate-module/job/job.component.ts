4d8f7bb8e7ea4c2694791c53c68c75ac0c35d0b3
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-01-09 17:57:49 +0500

    modify candidate currency

--- client/src/app/candidate-module/job/job.component.ts
+++ client/src/app/candidate-module/job/job.component.ts
@@ -472,14 +472,15 @@ export class JobComponent implements OnInit,AfterViewInit {
       this.count++;
     }
 
-    if(!this.current_salary && this.current_currency) {
+    if(!this.current_salary && this.current_currency !== "-1") {
       this.current_sal_log = "Please enter current base salary";
       this.count++;
     }
+    if((!this.current_salary && !this.current_currency) || (!this.current_salary && this.current_currency === "-1")){
+      this.count = 0;
+    }
 
-	if((!this.current_salary && !this.current_currency) || (!this.current_salary && this.current_currency === "-1")){
-		this.count = 0;
-	}
+
     if( this.count === 0 && this.selectedcountry.length>0 && this.jobselected.length>0 && this.base_currency && this.salary && this.selectedValue.length > 0 && this.availability_day)
     {
       this.authenticationService.job(this.currentUser._creator,f.value)
