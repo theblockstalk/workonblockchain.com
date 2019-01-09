9c196ed56bed03306d72e67d6fc8887f110b796d
Jack Tanner <jack@jackandtheblockstalk.com>
2019-01-09 22:48:33 +0000

    implemented part of controller

--- server/controller/api-v2/messages/post.controller.js
+++ server/controller/api-v2/messages/post.controller.js
@@ -2,7 +2,7 @@ const auth = require('../../middleware/auth-v2');
 const Schema = require('mongoose').Schema;
 const enumerations = require('../../../model/enumerations');
 const errors = require('../../services/errors');
-// const messages = require('../../../model/mongoose/users'); // TODO need to change to messages schema
+const messages = require('../../../model/mongoose/users'); // TODO need to change to messages schema
 
 module.exports.request = {
     type: 'post',
@@ -20,19 +20,15 @@ const bodySchema = new Schema({
         required: true
     },
     message: {
-        // file: {
-        //     url: {
-        //         type: String,
-        //         required: true,
-        //         validate: regexes.url
-        //     }
-        // },
-        // normal: {
-        //     message: {
-        //         type: String,
-        //         required: true
-        //     }
-        // },
+        normal: {
+            type: new Schema({
+                message: {
+                    type: String,
+                    required: true
+                }
+            }),
+            required: false
+        },
         job_offer: {
             type: new Schema({
                 title: {
@@ -81,6 +77,7 @@ module.exports.endpoint = async function (req, res) {
 
     const body = req.body;
 
+    const userType = req.auth.user.type;
     const sender_id = req.auth.user._id;
     const receiver_id = body.user_id;
     let newMessage = {
@@ -93,22 +90,40 @@ module.exports.endpoint = async function (req, res) {
     };
 
     if (body.msg_tag === "file") {
-
+        // TODO ...
     }
     else if (body.msg_tag === "normal") {
-        // TODO create
+        let messageDoc;
+        if (userType === 'candidate') {
+            messageDoc = messages.findOne({
+                sender_id: sender_id,
+                receiver_id: receiver_id,
+                msg_tag: 'job_offer_accepted'
+            });
+        } else {
+            messageDoc = messages.findOne({
+                sender_id: receiver_id,
+                receiver_id: sender_id,
+                msg_tag: 'job_offer_accepted'
+            });
+        }
+        if (messageDoc) errors.throwError("Job offer has not been accepted", 400);
+
+        newMessage.message.normal = body.message.normal;
     }
     else if (body.msg_tag === "job_offer") {
-        // const messageDoc = messages.findOne({
-        //     sender_id: sender_id,
-        //     receiver_id: receiver_id,
-        //     msg_tag: 'job_offer'
-        // });
-        // if (messageDoc) errors.throwError("Job offer already sent", 400);
-        //
-        // newMessage.message.job_offer = body.message.job_offer;
-    }
-    // messages.insert(newMessage);
+        if (userType !== 'company') errors.throwError("Job offer can only be set by a company", 400);
 
-    res.send('This is a response');
+        const messageDoc = messages.findOne({
+            sender_id: sender_id,
+            receiver_id: receiver_id,
+            msg_tag: 'job_offer'
+        });
+        if (messageDoc) errors.throwError("Job offer already sent", 400);
+
+        newMessage.message.job_offer = body.message.job_offer;
+    }
+    const messageDoc = await messages.insert(newMessage);
+
+    res.send(messageDoc);
 };
