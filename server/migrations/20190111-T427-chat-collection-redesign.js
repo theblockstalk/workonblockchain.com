f69699b05ccf9979d4b6763da8982f7055cd9dfc
Jack Tanner <jack@jackandtheblockstalk.com>
2019-01-14 12:52:22 +0000

    fixed up use of mongoose helper

--- server/migrations/20190111-T427-chat-collection-redesign.js
+++ server/migrations/20190111-T427-chat-collection-redesign.js
@@ -1,9 +1,14 @@
-const Messages = require('../model/messages');
+const messages = require('../model/mongoose/messages');
 const Chat = require('../model/chat');
 const logger = require('../controller/services/logger');
 
 
 let totalDocsToProcess, totalProcessed = 0, totalModified = 0;
+
+function isValidValue(val) {
+    if (val && val !== null && val !== "" && val !== -1) return true;
+    return false;
+}
 
 // This function will perform the migration
 module.exports.up = async function() {
@@ -16,216 +21,103 @@ module.exports.up = async function() {
     for ( null ; chatDoc !== null; chatDoc = await chatCursor.next()) {
         totalProcessed++;
         logger.debug("(" + totalProcessed + "/" + totalDocsToProcess + ") Migrating chat doc: " + chatDoc._id.toString());
-        let oldMessage,newMessages;
+        let newMessageDoc, oldMessage;
+        if ( isValidValue(chatDoc.message) ) {
+            oldMessage = chatDoc.message.toString();
+            oldMessage = oldMessage.replace(/["']/g, "");
+        }
+
+        newMessageDoc = {
+            _id: chatDoc._id,
+            sender_id: chatDoc.sender_id,
+            receiver_id: chatDoc.receiver_id,
+            msg_tag: chatDoc.msg_tag,
+            is_read: chatDoc.is_read,
+            date_created: chatDoc.date_created,
+        };
+
         if(chatDoc.msg_tag === 'job_offer'){
-            newMessages = new Messages({
-                _id: chatDoc._id,
-                sender_id: chatDoc.sender_id,
-                receiver_id: chatDoc.receiver_id,
-                msg_tag: chatDoc.msg_tag,
-                is_read: chatDoc.is_read,
-                date_created: chatDoc.date_created,
-                message : {
-                    job_offer:{
-                        title: chatDoc.job_title,
-                        salary: chatDoc.salary,
-                        salary_currency: chatDoc.salary_currency,
-                        type: chatDoc.job_type,
-                        location: chatDoc.interview_location,
-                        description: chatDoc.description
-                    }
+            newMessageDoc.message = {
+                job_offer:{
+                    title: chatDoc.job_title,
+                    salary: chatDoc.salary,
+                    salary_currency: chatDoc.salary_currency,
+                    type: chatDoc.job_type,
+                    location: chatDoc.interview_location,
+                    description: chatDoc.description
                 }
-            });
+            };
         }
         else if(chatDoc.msg_tag === 'job_offer_accepted'){
-            oldMessage = chatDoc.message.toString();
-            oldMessage = oldMessage.replace(/["']/g, "");
-            newMessages = new Messages({
-                _id: chatDoc._id,
-                sender_id: chatDoc.sender_id,
-                receiver_id: chatDoc.receiver_id,
-                msg_tag: chatDoc.msg_tag,
-                is_read: chatDoc.is_read,
-                date_created: chatDoc.date_created,
-                message : {
-                    job_offer_accepted:{
-                        message: oldMessage
-                    }
+            newMessageDoc.message = {
+                job_offer_accepted:{
+                    message: oldMessage
                 }
-            });
+            };
         }
         else if(chatDoc.msg_tag === 'job_offer_rejected'){
-            oldMessage = chatDoc.message.toString();
-            oldMessage = oldMessage.replace(/["']/g, "");
-            newMessages = new Messages({
-                _id: chatDoc._id,
-                sender_id: chatDoc.sender_id,
-                receiver_id: chatDoc.receiver_id,
-                msg_tag: chatDoc.msg_tag,
-                is_read: chatDoc.is_read,
-                date_created: chatDoc.date_created,
-                message : {
-                    job_offer_rejected:{
-                        message: oldMessage
-                    }
+            newMessageDoc.message = {
+                job_offer_rejected:{
+                    message: oldMessage
                 }
-            });
+            };
         }
         else if(chatDoc.msg_tag === 'interview_offer'){
-            if(chatDoc.description){
-                newMessages = new Messages({
-                    _id: chatDoc._id,
-                    sender_id: chatDoc.sender_id,
-                    receiver_id: chatDoc.receiver_id,
-                    msg_tag: chatDoc.msg_tag,
-                    is_read: chatDoc.is_read,
-                    date_created: chatDoc.date_created,
-                    message : {
-                        interview_offer:{
-                            location: chatDoc.interview_location,
-                            description: chatDoc.description,
-                            date_time: chatDoc.interview_date_time
-                        }
-                    }
-                });
-            }
-            else{
-                newMessages = new Messages({
-                    _id: chatDoc._id,
-                    sender_id: chatDoc.sender_id,
-                    receiver_id: chatDoc.receiver_id,
-                    msg_tag: chatDoc.msg_tag,
-                    is_read: chatDoc.is_read,
-                    date_created: chatDoc.date_created,
-                    message : {
-                        interview_offer:{
-                            location: chatDoc.interview_location,
-                            date_time: chatDoc.interview_date_time
-                        }
-                    }
-                });
+            newMessageDoc.message = {
+                interview_offer:{
+                    location: chatDoc.interview_location,
+                    date_time: chatDoc.interview_date_time
+                }
+            };
+
+            if( isValidValue(chatDoc.description) ){
+                newMessageDoc.message.interview_offer.description = chatDoc.description;
             }
         }
         else if(chatDoc.msg_tag === 'employment_offer'){
-            if(chatDoc.file_name){
-                newMessages = new Messages({
-                    _id: chatDoc._id,
-                    sender_id: chatDoc.sender_id,
-                    receiver_id: chatDoc.receiver_id,
-                    msg_tag: chatDoc.msg_tag,
-                    is_read: chatDoc.is_read,
-                    date_created: chatDoc.date_created,
-                    message : {
-                        employment_offer:{
-                            title: chatDoc.job_title,
-                            salary: chatDoc.salary,
-                            salary_currency: chatDoc.salary_currency,
-                            type: chatDoc.job_type,
-                            start_date: chatDoc.date_of_joining,
-                            description: chatDoc.description,
-                            file_url: chatDoc.file_name
-                        }
-                    }
-                });
-            }
-            else{
-                newMessages = new Messages({
-                    _id: chatDoc._id,
-                    sender_id: chatDoc.sender_id,
-                    receiver_id: chatDoc.receiver_id,
-                    msg_tag: chatDoc.msg_tag,
-                    is_read: chatDoc.is_read,
-                    date_created: chatDoc.date_created,
-                    message : {
-                        employment_offer:{
-                            title: chatDoc.job_title,
-                            salary: chatDoc.salary,
-                            salary_currency: chatDoc.salary_currency,
-                            type: chatDoc.job_type,
-                            start_date: chatDoc.date_of_joining,
-                            description: chatDoc.description
-                        }
-                    }
-                });
+            newMessageDoc.message = {
+                employment_offer:{
+                    title: chatDoc.job_title,
+                    salary: chatDoc.salary,
+                    salary_currency: chatDoc.salary_currency,
+                    type: chatDoc.job_type,
+                    start_date: chatDoc.date_of_joining,
+                    description: chatDoc.description,
+                }
+            };
+
+            if( isValidValue(chatDoc.file_name) ){
+                newMessageDoc.message.file_url.description = chatDoc.file_name;
             }
 
         }
         else if(chatDoc.msg_tag === 'employment_offer_accepted'){
-            oldMessage = chatDoc.message.toString();
-            oldMessage = oldMessage.replace(/["']/g, "");
-            newMessages = new Messages({
-                _id: chatDoc._id,
-                sender_id: chatDoc.sender_id,
-                receiver_id: chatDoc.receiver_id,
-                msg_tag: chatDoc.msg_tag,
-                is_read: chatDoc.is_read,
-                date_created: chatDoc.date_created,
-                message : {
-                    employment_offer_accepted:{
-                        employment_offer_message_id: chatDoc.employment_offer_reference,
-                        message: oldMessage
-                    }
+            newMessageDoc.message = {
+                employment_offer_rejected:{
+                    employment_offer_message_id: chatDoc.employment_offer_reference,
+                    message: oldMessage
                 }
-            });
-        }
-        else if(chatDoc.msg_tag === 'employment_offer_rejected'){
-            oldMessage = chatDoc.message.toString();
-            oldMessage = oldMessage.replace(/["']/g, "");
-            newMessages = new Messages({
-                _id: chatDoc._id,
-                sender_id: chatDoc.sender_id,
-                receiver_id: chatDoc.receiver_id,
-                msg_tag: chatDoc.msg_tag,
-                is_read: chatDoc.is_read,
-                date_created: chatDoc.date_created,
-                message : {
-                    employment_offer_rejected:{
-                        employment_offer_message_id: chatDoc.employment_offer_reference,
-                        message: oldMessage
-                    }
-                }
-            });
+            };
         }
         else{
-            if(chatDoc.msg_tag === 'normal' && chatDoc.file_name){
-                newMessages = new Messages({
-                    _id: chatDoc._id,
-                    sender_id: chatDoc.sender_id,
-                    receiver_id: chatDoc.receiver_id,
-                    msg_tag: chatDoc.msg_tag,
-                    is_read: chatDoc.is_read,
-                    date_created: chatDoc.date_created,
-                    message : {
-                        file:{
-                            url: chatDoc.file_name
-                        }
+            if(chatDoc.msg_tag === 'normal' && isValidValue(chatDoc.file_name) ){
+                newMessageDoc.message = {
+                    file:{
+                        url: chatDoc.file_name
                     }
-                });
+                };
             }
             else{
-                oldMessage = chatDoc.message.toString();
-                oldMessage = oldMessage.replace(/["']/g, "");
-                newMessages = new Messages({
-                    _id: chatDoc._id,
-                    sender_id: chatDoc.sender_id,
-                    receiver_id: chatDoc.receiver_id,
-                    msg_tag: chatDoc.msg_tag,
-                    is_read: chatDoc.is_read,
-                    date_created: chatDoc.date_created,
-                    message : {
-                        normal:{
-                            message: oldMessage
-                        }
+                newMessageDoc.message = {
+                    normal:{
+                        message: oldMessage
                     }
-                });
+                };
             }
         }
-        if (newMessages) {
-            //unSet = {employment_offer_reference:1,file_name:1,is_job_offered:1,job_title: 1,salary:1,salary_currency:1,job_type:1,interview_location:1,interview_date_time:1,description:1,date_of_joining:1,sender_name: 1, receiver_name: 1,is_company_reply:1};
-            //updateObj["$unset"] = unSet;
-            logger.debug("message doc added", newMessages);
-            const newDocs = await newMessages.save();
-            //const update = await Messages.update({_id: chatDoc._id}, updateObj);
+        if (newMessageDoc) {
+            logger.debug("message doc added", newMessageDoc);
+            const newDocs = await messages.insert(newMessageDoc);
             if (newDocs) totalModified++;
         }
     }
@@ -233,8 +125,5 @@ module.exports.up = async function() {
 
 // This function will undo the migration
 module.exports.down = async function() {
-    //BasicDBObject document = new BasicDBObject();
-    //collection.deleteMany(document);
-    //let newMessages = new Messages({});
-    await Messages.find({}).remove();
+    await messages.deleteOne({});
 }
--- server/model/mongoose/messages.js
+++ server/model/mongoose/messages.js
@@ -1,9 +1,53 @@
 let Messages = require('../messages');
 
-module.exports.insert = async function insert(data) {
+module.exports.insert = async function (data) {
     let newDoc = new Messages(data);
 
     await newDoc.save();
 
     return newDoc._doc;
+}
+
+
+module.exports.findOne = async function (selector) {
+    return await Messages.findOne(selector).lean();
+}
+
+module.exports.findOneById = async function (id) {
+    return await Messages.findById(id).lean();
+}
+
+module.exports.update = async function (selector, updateObj) {
+    await Messages.findOneAndUpdate(selector, updateObj, { runValidators: true });
+}
+
+module.exports.deleteOne = async function (selector) {
+    await Messages.find(selector).remove();
+}
+
+module.exports.count = async function (selector) {
+    return new Promise ( function (resolve, reject) {
+        try {
+            Messages.count(selector, function (err1, result) {
+                if (err1) reject(err1);
+                resolve(result);
+            })
+        }
+        catch (err2) {
+            reject(err2);
+        }
+    })
+}
+
+module.exports.findWithCursor = async function (selector) {
+    return await Messages.find(selector).cursor();
+}
+
+module.exports.findAndIterate = async function (selector, fn) {
+    let cursor = await this.findWithCursor(selector);
+    let doc = await cursor.next();
+
+    for (null; doc !== null; doc = await cursor.next()) {
+        await fn(doc);
+    }
 }
