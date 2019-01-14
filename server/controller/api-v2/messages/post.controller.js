16f70537a8184e8c026cae76e0f2dee64f880f04
Jack Tanner <jack@jackandtheblockstalk.com>
2019-01-14 17:02:42 +0000

    finished post messages endpoint

--- server/controller/api-v2/messages/post.controller.js
+++ server/controller/api-v2/messages/post.controller.js
@@ -2,7 +2,7 @@ const auth = require('../../middleware/auth-v2');
 const Schema = require('mongoose').Schema;
 const enumerations = require('../../../model/enumerations');
 const errors = require('../../services/errors');
-const messages = require('../../../model/mongoose/users'); // TODO need to change to messages schema
+const messages = require('../../../model/mongoose/messages'); // TODO need to change to messages schema
 
 module.exports.request = {
     type: 'post',
@@ -10,7 +10,7 @@ module.exports.request = {
 };
 
 const bodySchema = new Schema({
-    user_id: {
+    receiver_id: {
         type: String,
         required: true
     },
@@ -63,8 +63,8 @@ module.exports.inputValidation = {
     body: bodySchema
 };
 
-module.exports.auth = async function(req) {
-    console.log('in auth')
+module.exports.auth = async(req) {
+    console.log('in auth');
     await auth.isValidUser(req);
 
     // if (req.query.admin) {
@@ -72,14 +72,36 @@ module.exports.auth = async function(req) {
     // }
 }
 
-module.exports.endpoint = async function (req, res) {
+const checkJobOfferAccepted = function (userType, sender_id, receiver_id) {
+    let messageDoc;
+    if (userType === 'candidate') {
+        messageDoc = await messages.findOne({
+            sender_id: sender_id,
+            receiver_id: receiver_id,
+            msg_tag: 'job_offer_accepted'
+        });
+    } else {
+        messageDoc = await messages.findOne({
+            sender_id: receiver_id,
+            receiver_id: sender_id,
+            msg_tag: 'job_offer_accepted'
+        });
+    }
+    if (messageDoc) errors.throwError("Job offer has not been accepted", 400);
+};
+
+const checkMessageSenderType = function (userType, expectedType) {
+    if (userType !== expectedType) errors.throwError("Message can only be sent by a " + expectedType, 400);
+}
+
+module.exports.endpoint = async (req, res) {
     console.log('in endpoint')
 
     const body = req.body;
 
     const userType = req.auth.user.type;
     const sender_id = req.auth.user._id;
-    const receiver_id = body.user_id;
+    const receiver_id = body.receiver_id;
     let newMessage = {
         sender_id: sender_id,
         receiver_id: receiver_id,
@@ -90,31 +112,21 @@ module.exports.endpoint = async function (req, res) {
     };
 
     if (body.msg_tag === "file") {
-        // TODO ...
+        checkJobOfferAccepted(userType, sender_id, receiver_id);
+
+        newMessage.message.file = {
+            // file_url: TODO
+        }
     }
     else if (body.msg_tag === "normal") {
-        let messageDoc;
-        if (userType === 'candidate') {
-            messageDoc = messages.findOne({
-                sender_id: sender_id,
-                receiver_id: receiver_id,
-                msg_tag: 'job_offer_accepted'
-            });
-        } else {
-            messageDoc = messages.findOne({
-                sender_id: receiver_id,
-                receiver_id: sender_id,
-                msg_tag: 'job_offer_accepted'
-            });
-        }
-        if (messageDoc) errors.throwError("Job offer has not been accepted", 400);
+        checkJobOfferAccepted(userType, sender_id, receiver_id);
 
         newMessage.message.normal = body.message.normal;
     }
     else if (body.msg_tag === "job_offer") {
-        if (userType !== 'company') errors.throwError("Job offer can only be set by a company", 400);
+        checkMessageSenderType(userType, 'company');
 
-        const messageDoc = messages.findOne({
+        const messageDoc = await messages.findOne({
             sender_id: sender_id,
             receiver_id: receiver_id,
             msg_tag: 'job_offer'
@@ -123,7 +135,99 @@ module.exports.endpoint = async function (req, res) {
 
         newMessage.message.job_offer = body.message.job_offer;
     }
+    else if (body.msg_tag === "job_offer_accepted") {
+        checkMessageSenderType(userType, 'candidate');
+
+        const messageDoc = await messages.findOne({
+            sender_id: sender_id,
+            receiver_id: receiver_id,
+            msg_tag: 'job_offer_accepted'
+        });
+        if (messageDoc) errors.throwError("Job offer already accepted", 400);
+
+        newMessage.message.job_offer_accepted = body.message.job_offer_accepted;
+    }
+    else if (body.msg_tag === "job_offer_rejected") {
+        checkMessageSenderType(userType, 'candidate');
+
+        const messageDoc = await messages.findOne( { $or: [{
+                sender_id: sender_id,
+                receiver_id: receiver_id,
+                msg_tag: 'job_offer_accepted'
+            }, {
+                sender_id: sender_id,
+                receiver_id: receiver_id,
+                msg_tag: 'job_offer_rejected'
+            }]});
+        if (messageDoc) errors.throwError("Job offer already accepted or rejected", 400);
+
+        newMessage.message.job_offer_rejected = body.message.job_offer_rejected;
+    }
+    else if (body.msg_tag === "interview_offer") {
+        checkMessageSenderType(userType, 'company');
+        checkJobOfferAccepted(userType, sender_id, receiver_id);
+
+        newMessage.message.interview_offer = body.message.interview_offer;
+    }
+    else if (body.msg_tag === "employment_offer") {
+        checkMessageSenderType(userType, 'company');
+        checkJobOfferAccepted(userType, sender_id, receiver_id);
+
+        const lastEmploymentOfferDoc = await messages.findWithCursor({
+            sender_id: sender_id,
+            receiver_id: receiver_id,
+            msg_tag: 'employment_offer'
+        }).sort({date_created: -1}).limit(1).lean();
+
+        if (lastEmploymentOfferDoc) {
+            const responseToOfferDoc = await messages.findOne({ $or: [{
+                    "message.employment_offer_accepted.employment_offer_id": lastEmploymentOfferDoc._id
+                },{
+                    "message.employment_offer_rejected.employment_offer_id": lastEmploymentOfferDoc._id
+            }]});
+            if (!responseToOfferDoc) {
+                errors.throwError("Last employment offer needs to be accepted or rejected before a new offer can be sent", 400);
+            }
+        }
+
+        newMessage.message.employment_offer = body.message.employment_offer;
+    }
+    else if (body.msg_tag === "employment_offer_accepted") {
+        checkMessageSenderType(userType, 'candidate');
+        checkJobOfferAccepted(userType, sender_id, receiver_id);
+
+        let messageDoc = await messages.findOne({
+            _id: messages.employment_offer_accepted.employment_offer_id
+        });
+        if (!messageDoc) errors.throwError("Employment offer not found", 400);
+
+        messageDoc = await messages.findOne({
+            msg_status: 'employment_offer_accepted',
+            "messages.employment_offer_accepted.employment_offer_id": messages.employment_offer_accepted.employment_offer_id
+        });
+        if (!messageDoc) errors.throwError("Employment offer has already been accepted", 400);
+
+        newMessage.message.employment_offer_accepted = body.message.employment_offer_accepted;
+    }
+    else if (body.msg_tag === "employment_offer_rejected") {
+        checkMessageSenderType(userType, 'candidate');
+        checkJobOfferAccepted(userType, sender_id, receiver_id);
+
+        let messageDoc = await messages.findOne({
+            _id: messages.employment_offer_rejected.employment_offer_id
+        });
+        if (!messageDoc) errors.throwError("Employment offer not found", 400);
+
+        messageDoc = await messages.findOne({
+            msg_status: 'employment_offer_rejected',
+            "messages.employment_offer_rejected.employment_offer_id": messages.employment_offer_rejected.employment_offer_id
+        });
+        if (!messageDoc) errors.throwError("Employment offer has already been rejected", 400);
+
+        newMessage.message.employment_offer_rejected = body.message.employment_offer_rejected;
+    }
+
     const messageDoc = await messages.insert(newMessage);
 
     res.send(messageDoc);
-};
+}
