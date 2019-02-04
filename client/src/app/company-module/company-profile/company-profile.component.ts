d24f9fb2f9ff150de2bc0035b2341c8b1ef1e0c5
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-02-04 17:52:15 +0500

    added console in company profile

--- client/src/app/company-module/company-profile/company-profile.component.ts
+++ client/src/app/company-module/company-profile/company-profile.component.ts
@@ -286,6 +286,7 @@ export class CompanyProfileComponent implements OnInit ,  AfterViewInit {
                     }
                     if(data['saved_searches']) {
                       this.saved_searche = data['saved_searches'];
+                      console.log(this.saved_searche);
                       if(data['saved_searches'][0].location)
                       {
                         for (let country1 of data['saved_searches'][0].location)
