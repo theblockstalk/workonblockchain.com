53769d786462160e100adebf9d2a94617f2a0a03
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-02-05 13:17:54 +0500

    modify get metrics endpoint

--- server/controller/api/users/admins/getMetrics.controller.js
+++ server/controller/api/users/admins/getMetrics.controller.js
@@ -44,14 +44,21 @@ module.exports = async function (req, res) {
             aggregateArray(aggregatedData.locations, candidate.locations, locationList);
             aggregateArray(aggregatedData.roles, candidate.roles, enumerations.workRoles);
             aggregateArray(aggregatedData.interestAreas, candidate.interest_areas, enumerations.workBlockchainInterests);
-            aggregateArray(aggregatedData.blockchain.experimented, candidate.blockchain.experimented_platforms, enumerations.blockchainPlatforms);
-            aggregateObjArray(programmingLanguagesCount, candidate.programming_languages, enumerations.programmingLanguages, "language");
-            aggregateObjArrayAggregate(programmingLanguagesAggregate, candidate.programming_languages, enumerations.programmingLanguages, "language", "exp_year");
-            aggregateObjArray(blockchainCommercialCount, candidate.blockchain.commercial_platforms, enumerations.blockchainPlatforms, "name");
-            aggregateObjArrayAggregate(blockchainCommercialAggregate, candidate.blockchain.commercial_platforms, enumerations.blockchainPlatforms, "name", "exp_year");
-            aggregateObjArray(blockchainSmartCount, candidate.blockchain.smart_contract_platforms, enumerations.blockchainPlatforms, "name");
-            aggregateObjArrayAggregate(blockchainSmartAggregate, candidate.blockchain.smart_contract_platforms, enumerations.blockchainPlatforms, "name", "exp_year");
-
+            if(candidate.blockchain.experimented_platforms) {
+                aggregateArray(aggregatedData.blockchain.experimented, candidate.blockchain.experimented_platforms, enumerations.blockchainPlatforms);
+            }
+            if(candidate.programming_languages) {
+                aggregateObjArray(programmingLanguagesCount, candidate.programming_languages, enumerations.programmingLanguages, "language");
+                aggregateObjArrayAggregate(programmingLanguagesAggregate, candidate.programming_languages, enumerations.programmingLanguages, "language", "exp_year");
+            }
+            if(candidate.blockchain.commercial_platforms) {
+                aggregateObjArray(blockchainCommercialCount, candidate.blockchain.commercial_platforms, enumerations.blockchainPlatforms, "name");
+                aggregateObjArrayAggregate(blockchainCommercialAggregate, candidate.blockchain.commercial_platforms, enumerations.blockchainPlatforms, "name", "exp_year");
+            }
+            if(candidate.blockchain.smart_contract_platforms) {
+                aggregateObjArray(blockchainSmartCount, candidate.blockchain.smart_contract_platforms, enumerations.blockchainPlatforms, "name");
+                aggregateObjArrayAggregate(blockchainSmartAggregate, candidate.blockchain.smart_contract_platforms, enumerations.blockchainPlatforms, "name", "exp_year");
+            }
         }
         if (userDoc.terms) agreedTerms++;
 
