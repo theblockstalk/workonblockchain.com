d62edfdb16187c1eacd61eb0f5729af4f6ed700d
Tayyab Hussain <tayyab.hussain@mwanmobile.com>
2019-02-07 17:17:22 +0500

    fixed issues

--- server/controller/api/users/referrals/getReferralCodeForUsers.controller.js
+++ server/controller/api/users/referrals/getReferralCodeForUsers.controller.js
@@ -10,12 +10,26 @@ module.exports = async function(req, res) {
     }
     else {
         let token = crypto.getRandomString(10);
-        token = token.replace('+', '1').replace('-',2).replace('/',3).replace('*',4).replace('#',5);
-        const newDoc = await mongooseReferral.insert({
-            email : req.body.email,
-            url_token : token,
-            date_created: new Date(),
-        });
-		res.send(newDoc);
+        const uniqueToken = await isPrime(token);
+        if(uniqueToken) {
+            const newDoc = await mongooseReferral.insert({
+                email: req.body.email,
+                url_token: uniqueToken,
+                date_created: new Date(),
+            });
+            res.send(newDoc);
+        }
     }
 };
+
+async function isPrime(token){
+    const newDoc = await mongooseReferral.findOne({url_token : token});
+    if(newDoc) {
+        let newToken = crypto.getRandomString(10);
+        newToken = newToken.replace('+', '1').replace('-',2).replace('/',3).replace('*',4).replace('#',5).replace('=',6);
+        return isPrime(newToken);
+    }
+    else{
+        return token;
+    }
+}
