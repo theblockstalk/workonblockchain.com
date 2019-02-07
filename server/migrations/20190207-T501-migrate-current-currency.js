075d4098ba8ca51badd115c1645cff05e52ae4a3
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-02-07 16:29:20 +0500

    modify migration script

--- server/migrations/20190207-T501-migrate-current-currency.js
+++ server/migrations/20190207-T501-migrate-current-currency.js
@@ -4,21 +4,15 @@ const logger = require('../controller/services/logger');
 let totalDocsToProcess=0, totalProcessed = 0;
 
 module.exports.up = async function() {
-    totalDocsToProcess =await users.count({
-        type : 'candidate',
-        'candidate.current_currency': {$exists: false} ,
-        'candidate.current_salary': {$exists: true}});
+    totalDocsToProcess =await users.count({type : 'candidate'});
     logger.debug(totalDocsToProcess);
     await users.findAndIterate({
         type : 'candidate',
         'candidate.current_currency': {$exists: false} ,
         'candidate.current_salary': {$exists: true}}, async function(userDoc) {
-
         logger.debug("User doc : " , userDoc.candidate);
-        if(userDoc.candidate.current_salary && !userDoc.candidate.current_currency) {
-            totalProcessed++;
-            await users.update({ _id: userDoc._id },{ $set: {'candidate.current_currency' : userDoc.candidate.expected_salary_currency} });
-        }
+        totalProcessed++;
+        await users.update({ _id: userDoc._id },{ $set: {'candidate.current_currency' : userDoc.candidate.expected_salary_currency} });
 
     });
 
