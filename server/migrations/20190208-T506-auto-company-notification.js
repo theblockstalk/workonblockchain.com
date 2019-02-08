a0e5aa4a8de52e0d1453827213e0234ece411575
Jack Tanner <jack@jackandtheblockstalk.com>
2019-02-08 12:03:57 +0000

    new debugging tools

--- server/migrations/20190208-T506-auto-company-notification.js
+++ server/migrations/20190208-T506-auto-company-notification.js
@@ -4,11 +4,11 @@ const logger = require('../controller/services/logger');
 let totalDocsToProcess=0, totalProcessed = 0;
 
 module.exports.up = async function() {
-    totalDocsToProcess =await company.count({"candidates_sent_by_email.sent": { $gte : ISODate("2019-02-07T00:00:00.000+0000")} });
+    totalDocsToProcess =await company.count({"candidates_sent_by_email.sent": { $gte : new Date("2019-02-06")} });
     logger.debug(totalDocsToProcess);
 
-    await company.updateMany({"candidates_sent_by_email.sent": { $gte : ISODate("2019-02-06T00:00:00.000+0000")} },
-        {$pull: { candidates_sent_by_email: {sent: { $gte : ISODate("2019-02-06T00:00:00.000+0000")}}}});
+    await company.updateMany({"candidates_sent_by_email.sent": {$gte: new Date("2019-02-06")}},
+        {$pull: {candidates_sent_by_email: {sent: {$gte: new Date("2019-02-06")}}}});
 
     console.log('Total documents: ' + totalDocsToProcess);
     console.log('Total processed users: ' + totalProcessed);
