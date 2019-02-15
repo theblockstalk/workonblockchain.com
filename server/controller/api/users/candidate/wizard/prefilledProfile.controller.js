53f1e4381917b014be36a4c188f200d6bd5c3d31
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-02-15 14:43:14 +0500

    remove commented line

--- server/controller/api/users/candidate/wizard/prefilledProfile.controller.js
+++ server/controller/api/users/candidate/wizard/prefilledProfile.controller.js
@@ -11,8 +11,6 @@ module.exports = async function (req, res) {
     if(candidateUserDoc) {
         const userParam = req.body;
         let candidateUpdate = {}
-        //if (userParam.basics.first_name) candidateUpdate.first_name = userParam.basics.first_name;
-        //if (userParam.basics.last_name) candidateUpdate.last_name = userParam.basics.last_name;
         if (userParam.educationHistory) candidateUpdate['candidate.education_history'] = userParam.educationHistory;
         if (userParam.workHistory) candidateUpdate['candidate.work_history'] = userParam.workHistory;
         if (userParam.basics.summary) candidateUpdate['candidate.description'] = userParam.basics.summary;
