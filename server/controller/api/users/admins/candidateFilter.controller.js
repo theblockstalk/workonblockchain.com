3ca4d6e13d0fb4a4a69f5b150053d8a16a2b7f00
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-02-19 10:54:34 +0500

    modify BE candidate search

--- server/controller/api/users/admins/candidateFilter.controller.js
+++ server/controller/api/users/admins/candidateFilter.controller.js
@@ -6,11 +6,11 @@ module.exports = async function (req,res) {
     let queryBody = req.body;
 
     let filter = {};
-    if (queryBody.verify_status) filter.is_verify = queryBody.verify_status;
+    if (queryBody.verify_status) filter.is_verify = parseInt(queryBody.verify_status);
     if (queryBody.is_approve) filter.status = queryBody.is_approve;
-    if (queryBody.account_status) filter.disable_account = true;
-    if (!queryBody.account_status) filter.disable_account = false;
     if (queryBody.msg_tags) filter.msg_tags = queryBody.msg_tags;
+    if (queryBody.account_status === 'true') filter.disable_account = true;
+    else filter.disable_account = false;
 
     let search = {};
     if (queryBody.word) {
