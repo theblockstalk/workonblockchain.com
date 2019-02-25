1d3a80542f3dbb285f4a465faf314db96a5fac11
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-02-25 17:28:25 +0500

    write script down fucntion

--- server/migrations/20190225-T514-multiple-saved_searches.js
+++ server/migrations/20190225-T514-multiple-saved_searches.js
@@ -14,7 +14,7 @@ module.exports.up = async function() {
         logger.debug("company doc id: " , companyDoc._id);
         let set = {};
         let unset = {};
-        set.search_name = 'Job search';
+        set.saved_searches[0].search_name = 'Job search';
         set.when_receive_email_notitfications = companyDoc.saved_searches[0].when_receive_email_notitfications;
         unset.saved_searches[0].when_receive_email_notitfications = 1;
         logger.debug("set object" , set);
@@ -31,4 +31,25 @@ module.exports.up = async function() {
 
 module.exports.down = async function() {
 
+    totalDocsToProcess = await companies.count({saved_searches : { $exists: true, $ne : [] }});
+    logger.debug(totalDocsToProcess);
+
+    await companies.findAndIterate({saved_searches : { $exists: true, $ne : [] }}, async function(companyDoc) {
+        totalProcessed++;
+        logger.debug("company doc id: " , companyDoc._id);
+        let set = {};
+        let unset = {};
+        set.saved_searches[0].when_receive_email_notitfications = companyDoc.when_receive_email_notitfications;
+        unset.saved_searches[0].search_name = 1;
+        unset.when_receive_email_notitfications = 1;
+        logger.debug("set object" , set);
+        logger.debug("unset object" , unset);
+        await companies.update({_creator : companyDoc._creator},{$set : set, $unset: unset});
+        totalModified++;
+
+    });
+
+    console.log('Total user document to process: ' + totalDocsToProcess);
+    console.log('Total user processed document: ' + totalProcessed);
+    console.log('Total user modified document: ' + totalModified);
 }
