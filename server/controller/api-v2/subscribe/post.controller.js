8d94403f1e3f432a3c89cce566228ab671dbbe75
Tayyab Hussain <tayyab.hussain@mwanmobile.com>
2019-03-01 11:03:02 +0500

    fixed issues mentioned by Jack in PR

--- server/controller/api-v2/subscribe/post.controller.js
+++ server/controller/api-v2/subscribe/post.controller.js
@@ -25,65 +25,27 @@ module.exports.inputValidation = {
     body: bodySchema
 };
 
-async function getList(listName) {
-    let lists = await sendGrid.getAllLists();
-    console.log(lists);
-
-    for (const list of lists.lists) {
-        if (list.name === listName) {
-            return list;
-        }
-    }
-}
-
 module.exports.endpoint = async function (req, res) {
-    console.log('in endpoint');
     let added = 0, updated = 0, errors = 0;
-    const list = await getList('subscriber');
-    console.log(list);
+    const list = await sendGrid.getList('subscribers');
     const listId = list.id;
-    const updateResponse = await sendGrid.updateRecipient({
-        email: req.body.email,
-        user: "false"
-    });
 
     const recipientUpdate = {
         email: req.body.email,
         first_name: req.body.first_name,
-        last_name: req.body.last_name};
+        last_name: req.body.last_name
+    };
     await updateSendGridRecipient(listId, recipientUpdate);
-
-    async function updateSendGridRecipient(listId, recipientUpdate) {
-        try {
-            logger.debug('Updating sendgrid recipient', recipientUpdate);
-
-            const updateResponse = await sendGrid.updateRecipient(recipientUpdate);
-
-            added = added + updateResponse.new_count;
-            updated = updated + updateResponse.updated_count;
-            errors = errors + updateResponse.error_count;
-
-            if (updateResponse.error_count > 0) {
-                logger.error("Error updating user", {
-                    update: recipientUpdate,
-                    response: updateResponse
-                });
-            }
-
-            const recipientId = updateResponse.persisted_recipients[0];
-            await sendGrid.addRecipientToList(listId, recipientId);
-        } catch (error) {
-            logger.error(error.message, {
-                stack: error.stack,
-                name: error.name
-            });
-            errors = errors + 1;
-        }
+    const updateResponse = await sendgrid.updateRecipient(recipientUpdate);
+    if(updateResponse.error_count > 0) {
+        throw new Error(JSON.stringify({
+            message: "Error updating subscriber",
+            update: recipientUpdate,
+            response: updateResponse
+        }, null, 1))
     }
+    const recipientId = updateResponse.persisted_recipients[0];
+    await sendgrid.addRecipientToList(listId, recipientId);
 
-    res.send({
-        added: added,
-        updated: updated,
-        errors: errors
-    });
+    res.send()
 }
