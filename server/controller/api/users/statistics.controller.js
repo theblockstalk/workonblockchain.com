32365742d524acd395c06643758a77b4874fe3c8
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-03-04 11:57:44 +0500

    modify statistics endpoint

--- server/controller/api/users/statistics.controller.js
+++ server/controller/api/users/statistics.controller.js
@@ -2,13 +2,14 @@ const users = require('../../../model/mongoose/users');
 const logger = require('../../services/logger');
 
 module.exports = async function (req, res) {
-    const approvedUserCount = await users.count({type: 'candidate', "candidate.status.0.status" :'approved',
+    const approvedUserCount = await users.count({type: 'candidate', "candidate.history.0.status.status" :'approved',
         disable_account : false, is_verify : 1});
+    console.log(approvedUserCount);
 
-    const blockchainExperienceCount = await users.count({type: 'candidate', "candidate.status.0.status" :'approved',
+    const blockchainExperienceCount = await users.count({type: 'candidate', "candidate.history.0.status.status" :'approved',
         disable_account : false, is_verify : 1 ,
         $or: [{'candidate.blockchain.commercial_platforms': {$exists: true}, 'candidate.blockchain.smart_contract_platforms': {$exists: true}}]});
-
+    console.log(blockchainExperienceCount);
     res.send({
         approvedUsers :approvedUserCount,
         blockchainExperienceUsers : blockchainExperienceCount
