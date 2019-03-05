4048918e7c26feb1d45913f4d2f29d029f91ebc0
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-03-05 10:50:22 +0500

    BE fixes

--- server/controller/api/users/admins/getMetrics.controller.js
+++ server/controller/api/users/admins/getMetrics.controller.js
@@ -36,7 +36,14 @@ module.exports = async function (req, res) {
         candidate = userDoc.candidate;
         if (userDoc.is_verify) emailVerified++;
         if (userDoc.disable_account) dissabled++;
-        if (candidate.history[0].status.status === 'approved' && !userDoc.disable_account) {
+        let candidateStatus;
+        let candidateHistory = userDoc.candidate.history;
+        for(let history of candidateHistory) {
+            if(!candidateStatus) {
+                candidateStatus = history.status;
+            }
+        }
+        if (candidateStatus.status === 'approved' && !userDoc.disable_account) {
             approved++;
             if (candidate.expected_salary && candidate.expected_salary_currency) salaryList(salaryArray, candidate.expected_salary, candidate.expected_salary_currency)
             aggregateField(aggregatedData.nationality, userDoc.nationality, enumerations.nationalities);
