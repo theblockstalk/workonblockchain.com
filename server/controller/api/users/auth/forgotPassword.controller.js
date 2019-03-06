e3f778c48edc03a78bf38c69672f11faadf35fa6
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-03-06 17:14:40 +0500

    modify forgot password controller

--- server/controller/api/users/auth/forgotPassword.controller.js
+++ server/controller/api/users/auth/forgotPassword.controller.js
@@ -11,47 +11,35 @@ module.exports = async function (req,res) {
 
     const userDoc  = await users.findOneByEmail( queryBody.email );
     if(userDoc) {
-        if(userDoc.social_type === 'GOOGLE')
-        {
-            errors.throwError("Please log into your account using the 'Log in with Google' button", 400);
-        }
+        let signOptions = {
+            expiresIn:  "1h",
+        };
+        let forgotPasswordToken = jwtToken.createJwtToken(userDoc , signOptions);
+        await users.update({ _id: userDoc._id },{ $set: {'forgot_password_key': forgotPasswordToken } });
+        if(userDoc.type === 'candidate') {
+            let name;
+            const candidateDoc = await users.findOneById( userDoc._id);
+            if(candidateDoc && candidateDoc.first_name) {
+                name = candidateDoc.first_name;
 
-        else if(userDoc.social_type === 'LINKEDIN')
-        {
-            errors.throwError("Please log into your account using the 'Log in with LinkedIn' button", 400);
-        }
-
-        else {
-            let signOptions = {
-                expiresIn:  "1h",
-            };
-            let forgotPasswordToken = jwtToken.createJwtToken(userDoc , signOptions);
-            await users.update({ _id: userDoc._id },{ $set: {'forgot_password_key': forgotPasswordToken } });
-            if(userDoc.type === 'candidate') {
-                let name;
-                const candidateDoc = await users.findOneById( userDoc._id);
-                if(candidateDoc && candidateDoc.first_name) {
-                    name = candidateDoc.first_name;
-
-                }
-
-                forgotPasswordEmail.sendEmail(userDoc.email, name, forgotPasswordToken);
-                res.send({
-                    success : true
-                })
             }
-            if(userDoc.type === 'company') {
-                let name;
-                const companyDoc = await companies.findOne({_creator : userDoc._id});
-                if(companyDoc ) {
-                    name = companyDoc.first_name;
-                }
 
-                forgotPasswordEmail.sendEmail(userDoc.email, name, forgotPasswordToken);
-                res.send({
-                    success : true
-                })
+            forgotPasswordEmail.sendEmail(userDoc.email, name, forgotPasswordToken);
+            res.send({
+                success : true
+            })
+        }
+        if(userDoc.type === 'company') {
+            let name;
+            const companyDoc = await companies.findOne({_creator : userDoc._id});
+            if(companyDoc ) {
+                name = companyDoc.first_name;
             }
+
+            forgotPasswordEmail.sendEmail(userDoc.email, name, forgotPasswordToken);
+            res.send({
+                success : true
+            })
         }
     }
     else {
