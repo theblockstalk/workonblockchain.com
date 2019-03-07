d092bab29521576c47b223f1a4a659efb2958179
Jack Tanner <jack@jackandtheblockstalk.com>
2019-03-07 15:02:21 +0000

    installed amplitude package

--- server/controller/api/healthCheck.controller.js
+++ server/controller/api/healthCheck.controller.js
@@ -1,7 +1,8 @@
 const version = require('../../config/version.json').version;
 const errors = require('../services/errors');
+const amplitudeData = require('../services/amplitude');
 
-module.exports = function healthCheck(req, res) {
+module.exports = function (req, res) {
     if (req.query && req.query.error) {
         if (req.query.raw) {
             throw new Error("I am a normal error")
@@ -9,6 +10,20 @@ module.exports = function healthCheck(req, res) {
             errors.throwError("I am an application error", 400);
         }
     }
+
+    const amplitudeData = {
+        event_type: 'GET /',
+        user_id: '1234',
+        session_id: new Date().getTime(),
+        event_properties: {
+            //...
+        },
+        // user_properties: {
+        //     //...
+        // }
+    };
+    amplitude.track(amplitudeData)
+
     res.json({
         success: true,
         message: "this is a health check for the API",
