9211bce5c0e48ce4410c69b9603c014f6a6d717f
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-03-07 17:06:01 +0500

    update getCurrent controller

--- server/controller/api/users/candidate/getCurrent.controller.js
+++ server/controller/api/users/candidate/getCurrent.controller.js
@@ -1,18 +1,21 @@
 const users = require('../../../../model/mongoose/users');
 const filterReturnData = require('../filterReturnData');
 const errors = require('../../../services/errors');
+const crypto = require('crypto');
 
 module.exports = async function (req, res) {
     const myUserDoc = req.auth.user;
     if(String(myUserDoc._id) === req.params._id || myUserDoc.is_admin === 1) {
         const userDoc = await users.findByIdAndPopulate(req.params._id);
         if(userDoc) {
-            const filterData = filterReturnData.removeSensativeData(userDoc);
             let hash = crypto.createHmac('sha512', userDoc.salt);
-            hash.update('');
+            hash.update("");
             let hashedPasswordAndSalt = hash.digest('hex');
-            if (hashedPasswordAndSalt === userDoc.password_hash) filterData.password = false;
+            let password = true;
+            if (hashedPasswordAndSalt === userDoc.password_hash) password = false;
 
+            const filterData = filterReturnData.removeSensativeData(userDoc);
+            filterData.password = password;
             res.send(filterData);
         }
         else {
