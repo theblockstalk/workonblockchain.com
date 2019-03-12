9829c50ae0cfb22842c97a8ecf5212b31e562a3b
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-03-12 14:22:39 +0500

    update email template

--- server/controller/services/email/emails/candidateHistory.js
+++ server/controller/services/email/emails/candidateHistory.js
@@ -2,14 +2,8 @@ const emails = require('../emails');
 const settings = require('../../../../settings');
 
 module.exports.sendEmail = function sendEmail(email,name,emailSubject,email_text,isAccountDisabed) {
-    const sendTo = {
-        email:email
-    };
     const subject = emailSubject;
-
-    const sendToArray = [sendTo];
-
-
+ 
     const sendGridOptions = {
         templateId: "b07d4ca90da44fb2843d59796edc0dd3",
         subject: subject,
