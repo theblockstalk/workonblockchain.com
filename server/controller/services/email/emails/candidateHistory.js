14740e9604caae60deb72ab5b643fe795bf3d71b
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-03-12 15:53:42 +0500

    update email template id

--- server/controller/services/email/emails/candidateHistory.js
+++ server/controller/services/email/emails/candidateHistory.js
@@ -5,7 +5,7 @@ module.exports.sendEmail = function sendEmail(email,name,emailSubject,email_text
     const subject = emailSubject;
 
     const sendGridOptions = {
-        templateId: "b07d4ca90da44fb2843d59796edc0dd3",
+        templateId: "d8830894f2ac46d6af1b57abdf8d4ac5",
         subject: subject,
         from: {
             name: settings.SENDGRID.ACCOUNT_FROM_NAME,
