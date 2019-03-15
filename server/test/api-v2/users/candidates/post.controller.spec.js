4c0b5c9c9e38c45ec66af211a581e7537af95ac4
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-03-15 15:47:57 +0500

    update test cases

--- server/test/api-v2/users/candidates/post.controller.spec.js
+++ server/test/api-v2/users/candidates/post.controller.spec.js
@@ -3,7 +3,7 @@ const chaiHttp = require('chai-http');
 const crypto = require('crypto');
 const server = require('../../../../server');
 const mongo = require('../../../helpers/mongo');
-const users = require('../../../../model/users');
+const users = require('../../../../model/mongoose/users');
 const docGenerator = require('../../../helpers/docGenerator-v2');
 const candidateHelper = require('./candidateHelpers');
 const userHelper = require('../../../api/users/usersHelpers');
@@ -23,7 +23,27 @@ describe('update candidate profile', function () {
     describe('post /users/candidates', () => {
 
         it('it should create candidate profile', async () => {
+        const candidate = docGenerator.candidate();
 
+        const res = await candidateHelper.signupCandidate(candidate);
+
+        res.should.have.status(200);
+
+        const userDoc = await users.findOneByEmail(candidate.email);
+        userDoc.email.should.equal(candidate.email);
+        userDoc.is_verify.should.equal(0);
+        userDoc.is_approved.should.equal(0);
+        userDoc.is_admin.should.equal(0);
+        userDoc.disable_account.should.equal(false);
+        userDoc.type.should.equal("candidate");
+        should.exist(userDoc.jwt_token)
+
+        const salt = userDoc.salt;
+        let hash = crypto.createHmac('sha512', salt);
+        hash.update(candidate.password);
+        const hashedPasswordAndSalt = hash.digest('hex');
+        userDoc.password_hash.should.equal(hashedPasswordAndSalt);
+        userDoc.marketing_emails.should.equal(false);
 
     })
 })
