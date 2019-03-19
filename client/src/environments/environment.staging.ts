0dda9731707cc6cad2b194dda2845ac443dd95c9
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-03-19 17:56:53 +0500

    added id's in environment

--- client/src/environments/environment.staging.ts
+++ client/src/environments/environment.staging.ts
@@ -2,7 +2,9 @@ export const environment = {
   production: true,
   backend_url: 'https://staging-api.workonblockchain.com/',
   frontend_url: 'https://staging.workonblockchain.com/',
-  google_id: '699068166900-h9dp76b47mh4biq33e8c31ju8tvfgvqj.apps.googleusercontent.com',
-  linkedin_id : '86q3xso6dxvymx',
+  google_client_id : '699068166900-r52plq8abcks3d2sanfisp6a4i8hoem2.apps.googleusercontent.com',
+  linkedin_id: '860wydxdy7qqd2',
+  linkedin_redirect_url : 'https://staging.workonblockchain.com/linkedin-auth',
+  google_redirect_url : 'https://staging.workonblockchain.com/google-auth'
 };
 //console.log(environment);
