6304bd2413bd245586f1636c9d1cc86ff5f20f5e
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-03-19 18:00:35 +0500

    added id's in environment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   backend_url:'https://production-api.workonblockchain.com/',
   frontend_url: 'https://workonblockchain.com/',
-  google_client_id : '699068166900-r52plq8abcks3d2sanfisp6a4i8hoem2.apps.googleusercontent.com',
-  linkedin_id: '860wydxdy7qqd2',
+  google_client_id : '699068166900-296254o3gj8i3vcvfojp4pbqselaav9k.apps.googleusercontent.com',
+  linkedin_id: '861pkvi3iz4wp2',
   linkedin_redirect_url : 'https://workonblockchain.com/linkedin-auth',
   google_redirect_url : 'https://workonblockchain.com/google-auth'
 
