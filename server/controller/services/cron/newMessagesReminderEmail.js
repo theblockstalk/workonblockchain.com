c327f99fcc3ec8444d3d262256c1b19813032f40
Tayyab Hussain <tayyab.hussain@mwanmobile.org>
2019-04-01 17:00:16 +0500

    added code

--- server/controller/services/cron/newMessagesReminderEmail.js
+++ server/controller/services/cron/newMessagesReminderEmail.js
@@ -7,14 +7,22 @@ const logger = require('../logger');
 module.exports = async function () {
     logger.debug('Running new messages cron');
 
-    let userDoc = await users.find({
+    var currentTime = new Date();
+    console.log(currentTime);
+    currentTime.setSeconds(currentTime.getSeconds() + 20);
+    console.log(currentTime); //+20 secs
+    currentTime.setHours(currentTime.getHours() + 1);
+    console.log(currentTime); //+1 hour
+    process.exit();
+
+    /*let userDoc = await users.find({
         "conversations": {
             "$elemMatch":{
                 "unread_count":{$gte:0},
                 "last_message": {$gte: ISODate("2019-04-01T10:36:43.626Z")}, //+20 secs
-                "last_message": {$lt: ISODate("2019-04-01T10:36:43.626Z")} // +1 hour and 20 secs
+                "last_message": {$lt: ISODate("2019-04-01T10:36:43.626Z")}, // +1 hour and 20 secs
+                "last_message_reminder_email": {$gte: ISODate("2019-04-01T10:36:43.626Z")} // +1 hour and 20 secs
             },
-            "last_message_reminder_email": {$gte: ISODate("2019-04-01T10:36:43.626Z")} // +1 hour and 20 secs
         }, is_unread_msgs_to_send: true
     });
 
@@ -28,7 +36,7 @@ module.exports = async function () {
             if(companyDoc){
                 chatReminderEmail.sendEmail(userDoc[i].email, userDoc[i].disable_account, companyDoc.first_name);
             }
-        }*/
-    }
+        }
+    }*/
     logger.info('Unread chat messages email script was executed', {timestamp: Date.now()});
 }
