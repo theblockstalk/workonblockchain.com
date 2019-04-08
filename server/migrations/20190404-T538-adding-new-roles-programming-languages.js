000a68d2a932485c55f12cef0956647baccb9b29
Tayyab Hussain <tayyab.hussain@mwanmobile.org>
2019-04-08 10:55:02 +0500

    modifiying migration script for countries

--- server/migrations/20190404-T538-adding-new-roles-programming-languages.js
+++ server/migrations/20190404-T538-adding-new-roles-programming-languages.js
@@ -7,23 +7,37 @@ let totalDocsToProcess, totalModified = 0;
 // This function will perform the migration
 module.exports.up = async function() {
     let set = {};
-    totalDocsToProcess = await users.count({type:'candidate'});
+    totalDocsToProcess = await users.count();
     logger.debug(totalDocsToProcess);
 
-    await users.findAndIterate({type:'candidate'}, async function(candDoc) {
-        if(candDoc.nationality === 'Dutchman' || candDoc.nationality === 'Dutchwoman' ||
-            candDoc.nationality === 'Netherlander'){
-            logger.debug("processing user doc: ", {userId: candDoc._id});
-            set = {
-                nationality: "Dutch"
-            };
-            let updateObj = {$set: set};
+    await users.findAndIterate({}, async function(candDoc) {
+        if(candDoc.type === 'candidate'){
+            let updateObj = {};
+            if(candDoc.nationality === 'Dutchman' || candDoc.nationality === 'Dutchwoman' ||
+                candDoc.nationality === 'Netherlander'){
+                console.log(candDoc.nationality);
+                logger.debug("processing user doc: ", {userId: candDoc._id});
+                set = {
+                    nationality: "Dutch"
+                };
+                updateObj = {$set: set};
+            }
+            if(candDoc.candidate.base_country === 'Congo {Democratic Rep}'){
+                console.log(candDoc.candidate.base_country);
+                logger.debug("processing user doc: ", {userId: candDoc._id});
+                set = {
+                    "candidate.base_country": "Congo"
+                };
+                updateObj = {$set: set};
+            }
+
             if (updateObj) {
                 logger.debug("migrate user doc", set);
                 await users.update({_id: candDoc._id}, updateObj);
                 totalModified++;
             }
         }
+
     });
     logger.debug("total user doc processed", {total: totalModified});
 }
