495c8f63240e1637af937225d7020b23e786cc4b
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-04-17 17:53:57 +0500

    update migration script

--- server/migrations/20190401-T512-freelancers.js
+++ server/migrations/20190401-T512-freelancers.js
@@ -89,6 +89,10 @@ module.exports.up = async function() {
                     newSearch.name = newSearch.name + ' - freelancer';
                     newSearch.work_type = 'contractor';
                     delete newSearch.job_type;
+                    if(search.expected_annual_salary) {
+                        newSearch.expected_hourly_rate = Math.ceil((2*newSearch.expected_annual_salary)/(46*5*8));
+                        delete newSearch.expected_annual_salary;
+                    }
                     newSavedSearches.push(newSearch);
                 };
             }
