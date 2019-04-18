cd9b8605ccfbcee9251dde64e483c14e03655659
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-04-18 15:28:23 +0500

    added logger for health check debug

--- server/controller/middleware/errorHandler.js
+++ server/controller/middleware/errorHandler.js
@@ -3,6 +3,7 @@ const uuidv1 = require('uuid/v1');
 const { ApplicationError } = require('../services/errors');
 
 module.exports = function middleware(err, req, res, next) {
+    logger.debug("Error handler check request object 1: " , req);
     const requestID = uuidv1();
 
     let bug = {
@@ -13,13 +14,14 @@ module.exports = function middleware(err, req, res, next) {
         requestID: requestID,
         request: {}
     };
+    logger.debug("Error handler check request object 2: ", req);
     if (req.params) bug.request.params = req.params;
     if (req.query) bug.request.params = req.query;
     if (req.body) {
         if (req.body.password) delete req.body.password;
         bug.request.body = req.body;
     }
-
+    logger.debug("Error handler check bug object: ", bug);
     if(err instanceof ApplicationError) {
         logger.warn(err.message, bug);
     } else {
