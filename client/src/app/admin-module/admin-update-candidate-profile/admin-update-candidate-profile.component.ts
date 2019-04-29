40f269cb9ea098e9f2786d5acf3e1513da26abb9
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-04-29 18:02:48 +0500

    added Currency exchanges in blockchain skills option

--- client/src/app/admin-module/admin-update-candidate-profile/admin-update-candidate-profile.component.ts
+++ client/src/app/admin-module/admin-update-candidate-profile/admin-update-candidate-profile.component.ts
@@ -238,6 +238,11 @@ export class AdminUpdateCandidateProfileComponent implements OnInit,AfterViewIni
         if(a.name > b.name) { return 1; }
         return 0;
       })
+      this.otherSkills.sort(function(a, b){
+        if(a.name < b.name) { return -1; }
+        if(a.name > b.name) { return 1; }
+        return 0;
+      })
       this.authenticationService.getById(this.user_id)
         .subscribe(data =>
           {
