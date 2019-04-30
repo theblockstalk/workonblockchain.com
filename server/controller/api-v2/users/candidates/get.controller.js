df6dca28a8108467bf462e102f217e1001704eb9
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-04-30 12:40:19 +0500

    thinking about candidates get controller :P

--- server/controller/api-v2/users/candidates/get.controller.js
+++ server/controller/api-v2/users/candidates/get.controller.js
@@ -6,48 +6,26 @@ const filterReturnData = require('../../../api/users/filterReturnData');
 
 module.exports.request = {
     type: 'get',
-    path: '/users/:user_id/candidates'
+    path: '/users/candidates'
 };
-
-const paramSchema = new Schema({
-    user_id: String
-});
-
-const querySchema = new Schema({
-    admin: Boolean
-});
-
-module.exports.inputValidation = {
-    params: paramSchema,
-    query: querySchema
-}
 
 module.exports.auth = async function (req) {
     await auth.isLoggedIn(req);
 
-    if (req.query.admin) {
-        await auth.isAdmin(req);
-    }
 }
 
 module.exports.endpoint = async function (req, res) {
-    let userId;
-    if (req.query.admin) userId = req.params.user_id;
+    let filteredUsers = [];
+    await users.findAndIterate({type : 'candidate'}, async function(userDoc) {
+        filterReturnData.removeSensativeData(userDoc);
+        filteredUsers.push(userDoc);
+    });
 
-    else userId = req.auth.user._id;
-
-    const userDoc = await users.findByIdAndPopulate(userId);
-
-    if(userDoc) {
-        let password = true;
-        if (!userDoc.password_hash) password = false;
-        const filterData = filterReturnData.removeSensativeData(userDoc);
-        filterData.password = password;
-        console.log(filterData);
-        res.send(filterData);
+    if(filteredUsers && filteredUsers.length > 0) {
+        res.send(filteredUsers);
     }
+
     else {
-        errors.throwError("User not found", 404);
+        errors.throwError("No candidate exists", 404)
     }
-
 }
---
+++ server/controller/api-v2/users/candidates/user_id/get.controller.js
@@ -0,0 +1,50 @@
+const auth = require('../../../../middleware/auth-v2');
+const Schema = require('mongoose').Schema;
+const users = require('../../../../../model/mongoose/users');
+const errors = require('../../../../services/errors');
+const filterReturnData = require('../../../../api/users/filterReturnData');
+
+module.exports.request = {
+    type: 'get',
+    path: '/users/:user_id/candidates'
+};
+
+const paramSchema = new Schema({
+    user_id: String
+});
+
+const querySchema = new Schema({
+    admin: Boolean
+});
+
+module.exports.inputValidation = {
+    params: paramSchema,
+    query: querySchema
+}
+
+module.exports.auth = async function (req) {
+    await auth.isLoggedIn(req);
+
+    if (req.query.admin) {
+        await auth.isAdmin(req);
+    }
+}
+
+module.exports.endpoint = async function (req, res) {
+    let userId;
+    if (req.query.admin) userId = req.params.user_id;
+    else userId = req.auth.user._id;
+
+    const userDoc = await users.findByIdAndPopulate(userId);
+    if(userDoc) {
+        let password = true;
+        if (!userDoc.password_hash) password = false;
+        const filterData = filterReturnData.removeSensativeData(userDoc);
+        filterData.password = password;
+        console.log(filterData);
+        res.send(filterData);
+    }
+    else {
+        errors.throwError("User not found", 404);
+    }
+}
