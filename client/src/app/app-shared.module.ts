088676b2e9a8a5488c36c689c2eff71f58190f3d
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-05-09 09:41:02 +0500

    removed ngx-image from shared module

--- client/src/app/app-shared.module.ts
+++ client/src/app/app-shared.module.ts
@@ -2,12 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SafeHtmlPipe } from './pipe.safehtml';
-import { ImageCropperModule } from 'ngx-image-cropper';
 import { ImageCropperComponent } from "ngx-img-cropper";
 
 @NgModule({
-  imports:      [ CommonModule, ImageCropperModule  ],
+  imports:      [ CommonModule  ],
   declarations: [ SafeHtmlPipe, ImageCropperComponent],
-  exports:      [ SafeHtmlPipe, CommonModule, FormsModule , ImageCropperModule, ImageCropperComponent]
+  exports:      [ SafeHtmlPipe, CommonModule, FormsModule , ImageCropperComponent]
 })
 export class SharedModule { }
