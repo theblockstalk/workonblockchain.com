72f5bc91a33a93e60abbeea043430327f3827cff
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-05-09 09:38:57 +0500

    created email template component

---
+++ client/src/app/admin-editor-module/email-templates/email-templates.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-email-templates',
+  templateUrl: './email-templates.component.html',
+  styleUrls: ['./email-templates.component.css']
+})
+export class EmailTemplatesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
