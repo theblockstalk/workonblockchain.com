3c701b72401a14a472d97be061f26d2250da1005
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-05-09 15:48:04 +0500

    modify cropper settings

--- client/src/app/candidate-module/edit-candidate-profile/edit-candidate-profile.component.ts
+++ client/src/app/candidate-module/edit-candidate-profile/edit-candidate-profile.component.ts
@@ -163,6 +163,8 @@ export class EditCandidateProfileComponent implements OnInit,AfterViewInit {
     this.cropperSettings.canvasWidth = 300;
     this.cropperSettings.canvasHeight = 300;
     this.cropperSettings.rounded = true;
+    this.cropperSettings.cropperDrawSettings.strokeWidth = 2;
+    this.cropperSettings.cropperDrawSettings.strokeColor = 'black';
     this.imageCropData = {};
   }
 
