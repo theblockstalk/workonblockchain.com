71b374711e0ad97bf2bd6b936292636cb6898f3a
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-05-22 11:47:58 +0500

    some fixes in wizard component

--- client/src/app/L0-components/navbars/wizards/wizards.component.ts
+++ client/src/app/L0-components/navbars/wizards/wizards.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'L0-navbars-wizards',
+  selector: 'app-c-nav-wizard',
   templateUrl: './wizards.component.html',
   styleUrls: ['./wizards.component.css']
 })
 export class WizardsComponent implements OnInit {
-  @Input() wizardMenu = [];
+  @Input() items = [];
   constructor() { }
 
   ngOnInit() {
