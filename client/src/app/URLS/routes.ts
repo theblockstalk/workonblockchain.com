b2ff3e02055712873ca55f16fc322867a972869c
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-05-29 14:29:01 +0500

    remove uncommented code from routes.js

--- client/src/app/URLS/routes.ts
+++ client/src/app/URLS/routes.ts
@@ -1,12 +1,3 @@
-// export const talent_edit = function () {
-//   return 'users/talent/edit';
-// }
-//
-// export const admin_talent_edit = function (user_id) {
-//   return 'admins/talent/:user_id/edit';
-// }
-
-
 export const urls = {
   users_talent_edit: 'users/talent/edit',
   admins_talent_edit: 'admins/talent/edit/:user_id'
