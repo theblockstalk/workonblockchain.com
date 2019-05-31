e2578cc6067d155aeac04c785f5b17eca2665b90
Tayyab Hussain <tayyab.hussain@mwanmobile.org>
2019-05-31 15:17:24 +0500

    deleted routes.js

--- server/routes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('./controller/middleware/auth');
-const asyncMiddleware = require('./controller/middleware/asyncMiddleware');
-
-module.exports = router;
