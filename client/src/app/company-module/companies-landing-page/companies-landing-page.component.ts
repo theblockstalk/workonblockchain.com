ce0d30fb5447bdcc032b542516c5c2862faf5e80
Tayyab Hussain <tayyab.hussain@mwanmobile.org>
2019-06-10 15:16:35 +0500

    fixed issue on Comp landing page:

--- client/src/app/company-module/companies-landing-page/companies-landing-page.component.ts
+++ client/src/app/company-module/companies-landing-page/companies-landing-page.component.ts
@@ -47,8 +47,9 @@ export class CompaniesLandingPageComponent implements OnInit, AfterViewInit {
         data => {
           if(data)
           {
-            this.approvedUsers = data['approvedUsers'];
-            this.blockchainExperienceUsers = Math.floor((data['blockchainExperienceUsers'] / data['approvedUsers'])*100) + "%";
+            console.log(data);
+            this.approvedUsers = data['approvedEnabled']['count'];
+            this.blockchainExperienceUsers = Math.floor((data['approvedEnabled']['aggregated']['blockchain']['commercial'] / data['approvedEnabled']['count'])*100) + "%";
           }
         });
   }
