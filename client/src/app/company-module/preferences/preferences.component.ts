268daa492c1f9c2eaaf9b66e1401b6956998ef6f
Sadia Abbas <sadia.abbas@mwanmobile.com>
2019-06-13 12:22:26 +0500

    fixed company wzard

--- client/src/app/company-module/preferences/preferences.component.ts
+++ client/src/app/company-module/preferences/preferences.component.ts
@@ -268,6 +268,7 @@ export class PreferencesComponent implements OnInit, AfterViewInit, AfterViewChe
   exp_year_log;
   work_type_log;
   annual_salary_log;
+  hourly_log;
   checkNumber(salary) {
     return /^[0-9]*$/.test(salary);
   }
@@ -340,7 +341,7 @@ export class PreferencesComponent implements OnInit, AfterViewInit, AfterViewChe
           const checkNumber = this.checkNumber(this.preferncesForm.value.prefItems[i].expected_hourly_rate);
           if(checkNumber === false) {
             count = 1;
-            this.expected_hourly_rate_log = "Hourly rate should be a number "
+            this.hourly_log = "Hourly rate should be a number "
           }
         }
 
@@ -358,18 +359,16 @@ export class PreferencesComponent implements OnInit, AfterViewInit, AfterViewChe
         }
 
         if(this.preferncesForm.value.prefItems[i].work_type === 'contractor'){
+          if(!this.preferncesForm.value.prefItems[i].expected_hourly_rate) {
+            this.hourly_log = 'Please enter hourly rate';
+            count = 1;
+          }
           if(this.preferncesForm.value.prefItems[i].expected_hourly_rate){
             if(!this.preferncesForm.value.prefItems[i].currency || this.preferncesForm.value.prefItems[i].currency === 'Currency') {
               this.expected_hourly_rate_log = "Please choose currency ";
               count = 1;
             }
           }
-        }
-
-
-        if(this.preferncesForm.value.prefItems[i].work_type === 'contractor' && !this.preferncesForm.value.prefItems[i].expected_hourly_rate && this.preferncesForm.value.prefItems[i].currency) {
-          this.expected_hourly_rate_log = "Please enter expected hours ";
-          count = 1;
         }
 
         if(this.preferncesForm.value.prefItems[i].residence_country && this.preferncesForm.value.prefItems[i].residence_country.length > 50) {
