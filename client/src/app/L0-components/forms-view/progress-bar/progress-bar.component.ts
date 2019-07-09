c72b5d92915f731c50f3b4de8d9543b01a6d99df
Tayyab Hussain <tayyab.hussain@mwanmobile.org>
2019-07-09 10:30:44 +0500

    completed progress bar component

--- client/src/app/L0-components/forms-view/progress-bar/progress-bar.component.ts
+++ client/src/app/L0-components/forms-view/progress-bar/progress-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-c-formv-progress-bar',
@@ -8,10 +9,18 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ProgressBarComponent implements OnInit {
   @Input() value: number;
   @Input() class: string; //'success', 'warning', 'info'
+  @Input() routerUrl: string;
+  @Input() isAdmin: boolean; // true/false
+  sectionScroll;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  internalRoute(page,dst){
+    this.sectionScroll=dst;
+    this.router.navigate([page], {fragment: dst});
   }
 
 }
