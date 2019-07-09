e98760df57e9edb1fbadc70af52599f861a4c1cd
Tayyab Hussain <tayyab.hussain@mwanmobile.org>
2019-07-09 11:57:14 +0500

    added candidate-view component in L2-pages

--- client/src/app/L2-pages/candidate-view/candidate-view.component.ts
+++ client/src/app/L2-pages/candidate-view/candidate-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-p-candidate-view',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./candidate-view.component.css']
 })
 export class CandidateViewComponent implements OnInit {
+  @Input() userDoc: object;
+  @Input() viewBy: string; // "admin", "candidate", company
 
   constructor() { }
 
