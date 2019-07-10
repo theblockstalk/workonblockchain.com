5693e744eb15e54407132aee3591be953fe9c995
Tayyab Hussain <tayyab.hussain@mwanmobile.org>
2019-07-10 11:08:27 +0500

    fixed padding issue for note in candidate history

--- client/src/app/L0-components/forms-view/html-area/html-area.component.ts
+++ client/src/app/L0-components/forms-view/html-area/html-area.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HtmlAreaViewComponent implements OnInit {
   @Input() value: string;
+  @Input() class: string; //p-2 etc optional
+
   constructor() { }
 
   ngOnInit() {
