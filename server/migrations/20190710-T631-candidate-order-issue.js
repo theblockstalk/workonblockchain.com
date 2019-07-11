425e60169e5430488f9d8948629890322731ac53
Tayyab Hussain <tayyab.hussain@mwanmobile.org>
2019-07-11 12:19:11 +0500

    done migration script for latest_status time_stamp

--- server/migrations/20190710-T631-candidate-order-issue.js
+++ server/migrations/20190710-T631-candidate-order-issue.js
@@ -1,26 +1,37 @@
 const logger = require('../controller/services/logger');
 const users = require('../model/mongoose/users');
 const fs = require("fs");
-const log = require('log-to-file');
+const log = require('log-to-file'); //only for local usage
 
 let totalDocsToProcess, totalModified = 0, totalProcessed=0;
 
 // This function will perform the migration
 module.exports.up = async function() {
-    let totalCount = 0;
     totalDocsToProcess = await users.count({type: 'candidate'});
     logger.debug(totalDocsToProcess);
+
     await users.findAndIterate({type: 'candidate'}, async function (userDoc) {
         //console.log(userDoc.candidate.history[0].status);
+        totalProcessed++;
         if(!userDoc.candidate.history[0].timestamp || !userDoc.candidate.latest_status.timestamp){
-            totalCount++;
-            const data = 'it has no timestamp ['+totalCount+']: '+ userDoc._id;
+            const data = 'it has no timestamp ['+totalModified+']: '+ userDoc._id;
             console.log(data);
-            log(data, 'my-log.log');
+            console.log(userDoc.candidate.history[0].timestamp);
+            //process.exit();
+            let set = {};
+            set['candidate.latest_status.timestamp'] = userDoc.candidate.history[0].timestamp;
+            logger.debug("set object: " , set);
+
+            await users.update({_id : userDoc._id},{$set : set});
+            totalModified++;
+
+            log(data, 'T631.log'); //only for local usage
         }
     });
 
-    console.log("total: " + totalCount);
+    console.log('Total user document to process: ' + totalDocsToProcess);
+    console.log('Total user processed document: ' + totalProcessed);
+    console.log('Total user modified document: ' + totalModified);
 }
 
 // This function will undo the migration
