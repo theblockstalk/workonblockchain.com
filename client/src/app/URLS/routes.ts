f5adc4f65e7922cca5c8fbc4dad21ec0a40e130a
Tayyab Hussain <tayyab.hussain@mwanmobile.org>
2019-07-12 15:28:41 +0500

    done routes for candidate & compnay to view canddiate profile

--- client/src/app/URLS/routes.ts
+++ client/src/app/URLS/routes.ts
@@ -5,5 +5,5 @@ export const urls = {
   pages: 'pages/:page_name',
   admin_talent_view: 'admins/talent/:user_id/view',
   company_talent_view: 'company/talent/:user_id/view',
-  candidate_profile_view: 'candidate/candidate_profile',
+  candidate_profile_view: 'talent/view',
 };
