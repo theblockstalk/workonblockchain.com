cf8cfefb364073a034be3e19ab152dae948b25d0
Tayyab Hussain <tayyab.hussain@mwanmobile.org>
2019-07-19 15:37:22 +0500

    using L0 components

--- client/src/app/L0-components/forms-view/html-area/html-area.component.ts
+++ client/src/app/L0-components/forms-view/html-area/html-area.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class HtmlAreaViewComponent implements OnInit {
   @Input() value: string;
   @Input() class: string; //p-2 etc optional
+  @Input() label: string; //optional
 
   constructor() { }
 
