6eeb6b976e0b958d2f0bf9d010886fbde679d569
Tayyab Hussain <tayyab.hussain@mwanmobile.org>
2019-07-25 16:12:11 +0500

    fixed issues

--- client/src/app/L1-items/dashboard/dashboard.component.spec.ts
+++ client/src/app/L1-items/dashboard/dashboard.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TextValueComponent } from './dashboard.component';
+import { DashboardComponent } from './dashboard.component';
 
-describe('TextValueComponent', () => {
-  let component: TextValueComponent;
-  let fixture: ComponentFixture<TextValueComponent>;
+describe('DashboardComponent', () => {
+  let component: DashboardComponent;
+  let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TextValueComponent ]
+      declarations: [ DashboardComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TextValueComponent);
+    fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
