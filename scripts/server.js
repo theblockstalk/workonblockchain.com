20c727b3e0c714a06bbeb51a03e8a69dc36ac349
Jack Tanner <jack@jackandtheblockstalk.com>
2019-08-07 15:42:39 +0100

    all automated deploy script

--- scripts/server.js
+++ scripts/server.js
@@ -34,31 +34,31 @@ async function deployBackend(environmentName) {
     console.log('This script will deploy the latest in the /server directory to the backend application');
     console.log('Please make sure there is no files in the working directory (responsibly do a `git stash` if you are unsure)');
 
-    console.log();
-    console.log('(1/5) getting Git branch and commit info');
-    const gitInfo = await scriptHelpers.getGitCommit();
-    console.log(gitInfo);
+    // console.log();
+    // console.log('(1/5) getting Git branch and commit info');
+    // const gitInfo = await scriptHelpers.getGitCommit();
+    // console.log(gitInfo);
+    //
+    // scriptHelpers.checkGitBranch(gitInfo.branch, environmentName);
 
-    scriptHelpers.checkGitBranch(gitInfo.branch, environmentName);
+    // console.log();
+    // console.log('(2/5) creating distribution package from server/');
+    // const versonName = 'server_' + gitInfo.commit + '_' + environmentName;
+    // await scriptHelpers.createTempServerDir(tempServerDirName, environmentName);
+    // await scriptHelpers.addVersionFile(tempServerDirName + 'config/version.json', versonName);
+    // const zipFileName = await scriptHelpers.zipServerDir(tempDirName, tempServerDirName, versonName);
 
-    console.log();
-    console.log('(2/5) creating distribution package from server/');
-    const versonName = 'server_' + gitInfo.commit + '_' + environmentName;
-    await scriptHelpers.createTempServerDir(tempServerDirName, environmentName);
-    await scriptHelpers.addVersionFile(tempServerDirName + 'config/version.json', versonName);
-    const zipFileName = await scriptHelpers.zipServerDir(tempDirName, tempServerDirName, versonName);
+    // console.log(zipFileName);
+    // console.log('(3/5) uploading the environment version (distribution) to S3');
+    // let distributionS3File = await scriptHelpers.uploadZipfileToS3(envName, s3bucket, zipFileName);
+    // console.log(distributionS3File);
 
-    console.log(zipFileName);
-    console.log('(3/5) uploading the environment version (distribution) to S3');
-    let distributionS3File = await scriptHelpers.uploadZipfileToS3(envName, s3bucket, zipFileName);
-    console.log(distributionS3File);
+    // console.log();
+    // console.log('(4/5) creating a new application version');
+    // await scriptHelpers.addElasticEnvironmentVersion(s3bucket, appName, zipFileName, distributionS3File);
 
-    console.log();
-    console.log('(4/5) creating a new application version');
-    await scriptHelpers.addElasticEnvironmentVersion(s3bucket, appName, zipFileName, distributionS3File);
-
-    console.log();
-    console.log('(5/5) updating the elastic beanstalk environment');
-    const ebUpdate = await scriptHelpers.updateElisticEnvironment(appName, envName, zipFileName);
-    console.log(ebUpdate);
+    // console.log();
+    // console.log('(5/5) updating the elastic beanstalk environment');
+    // const ebUpdate = await scriptHelpers.updateElisticEnvironment(appName, envName, zipFileName);
+    // console.log(ebUpdate);
 }
