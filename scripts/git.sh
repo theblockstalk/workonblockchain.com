244be33dc73657d344b0a3e7a6cecf3fb6b79f9d
WOB <contact@workonblockchain.com>
2019-08-07 23:39:19 +0100

    git script

---
+++ scripts/git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git config --global user.name "WOB"
+git config --global user.email "contact@workonblockchain.com"
+git log --pretty=format:'%h' -n 1
+export GIT_HASH=$(git log --pretty=format:'%h' -n 1)
+export SERVER_VERSION="server_"$GIT_HASH"_$1"
+export SERVER_VERSION_FILE="{ \"version\": \""$SERVER_VERSION"\" }"
+export SERVER_ZIP_FILE="$SERVER_VERSION.zip"
+export CLIENT_VERSION="client_"$GIT_HASH"_$1"
+export CLIENT_VERSION_FILE="{ \"version\": \""$SERVER_VERSION"\" }"
