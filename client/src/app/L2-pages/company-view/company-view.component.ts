25c049eb83f900d5dcf1f85aac49c2b7a3ad9b1d
Tayyab Hussain <tayyab.hussain@mwanmobile.org>
2019-09-11 14:38:50 +0500

    done comp profile URL & page

--- client/src/app/L2-pages/company-view/company-view.component.ts
+++ client/src/app/L2-pages/company-view/company-view.component.ts
@@ -13,12 +13,71 @@ export class CompanyViewComponent implements OnInit {
   @Input() userDoc: object;
   @Input() viewBy: string; // admin, company
 
-  constructor() { }
+  companyMsgTitle;companyMsgBody;imgPath;referred_name;
+  pricePlanLink = '/pricing';company_name;countries; selectedValueArray = [];
+
+  constructor(private route: ActivatedRoute, private router: Router,private authenticationService: UserService) { }
 
   ngOnInit() {
     console.log(this.viewBy);
     console.log('in company view page level');
     console.log(this.userDoc);
+    if(this.userDoc['company_logo'] != null ) this.imgPath =  this.userDoc['company_logo'];
+    if (this.userDoc['name']) this.referred_name = this.userDoc['name'];
+    else if(this.userDoc['_creator'].referred_email) this.referred_name = this.userDoc['_creator'].referred_email;
+
+    this.company_name = this.userDoc['first_name'].charAt(0).toUpperCase()+''+this.userDoc['first_name'].slice(1)+' '+this.userDoc['last_name'].charAt(0).toUpperCase()+''+this.userDoc['last_name'].slice(1)
+    console.log(this.company_name);
+
+    if(this.viewBy === 'company'){
+      this.authenticationService.get_page_content('Company popup message')
+      .subscribe(
+        data => {
+          if(data){
+              this.companyMsgTitle = data['page_title'];
+              this.companyMsgBody = data['page_content'];
+          }
+        }
+      );
+    }
+  }
+
+  getLocation(location) {
+    this.selectedValueArray = [];
+    for (let country1 of location) {
+      let locObject : any = {};
+      if (country1['remote'] === true) {
+        this.selectedValueArray.push({name: 'Remote'});
+      }
+      if (country1['city']) {
+        let city = country1['city'].city + ", " + country1['city'].country;
+        locObject.name = city;
+        locObject.type = 'city';
+        this.selectedValueArray.push(locObject);
+      }
+    }
+    this.countries = this.selectedValueArray;
+    this.countries.sort(function(a, b){
+      if(a.name < b.name) { return -1; }
+      if(a.name > b.name) { return 1; }
+      return 0;
+    });
+    if(this.countries.find((obj => obj.name === 'Remote'))) {
+      let remoteValue = this.countries.find((obj => obj.name === 'Remote'));
+      this.countries.splice(0, 0, remoteValue);
+      this.countries = this.filter_array(this.countries);
+    }
+    console.log(this.countries);
+    return this.countries;
+  }
+
+  filter_array(arr) {
+    var hashTable = {};
+    return arr.filter(function (el) {
+      var key = JSON.stringify(el);
+      var match = Boolean(hashTable[key]);
+      return (match ? false : hashTable[key] = true);
+    });
   }
 
 }
