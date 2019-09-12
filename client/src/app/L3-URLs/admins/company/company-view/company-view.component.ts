8ff93b6b170f94e7c892b6c5cbe490d98034aea3
Tayyab Hussain <tayyab.hussain@mwanmobile.org>
2019-09-12 11:12:04 +0500

    done approve/disapprove functionality

--- client/src/app/L3-URLs/admins/company/company-view/company-view.component.ts
+++ client/src/app/L3-URLs/admins/company/company-view/company-view.component.ts
@@ -22,25 +22,26 @@ export class CompanyViewComponent implements OnInit {
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     this.admin_log = JSON.parse(localStorage.getItem('admin_log'));
 
-    if(this.user_id && this.admin_log.is_admin === 1 && this.currentUser) {
-      this.authenticationService.getCurrentCompany(this.user_id, true)
-        .subscribe(
-          data =>
-          {
-            this.companyDoc = data;
-          },
-          error =>
-          {
-            if(error['status'] === 404 && error['error']['message'] && error['error']['requestID'] && error['error']['success'] === false) {
-              this.error = error['error']['message'];
-            }
-            else if(error['status'] === 400 && error['error']['message'] && error['error']['requestID'] && error['error']['success'] === false) {
-              this.error = error['error']['message'];
-            }
-            else this.error = "Something went wrong";
-          });
+    if(this.currentUser) {
+      if (this.user_id && this.admin_log.is_admin === 1 && this.currentUser) {
+        this.authenticationService.getCurrentCompany(this.user_id, true)
+          .subscribe(
+            data => {
+              this.companyDoc = data;
+            },
+            error => {
+              if (error['status'] === 404 && error['error']['message'] && error['error']['requestID'] && error['error']['success'] === false) {
+                this.error = error['error']['message'];
+              }
+              else if (error['status'] === 400 && error['error']['message'] && error['error']['requestID'] && error['error']['success'] === false) {
+                this.error = error['error']['message'];
+              }
+              else this.error = "Something went wrong";
+            });
+      }
+      else this.router.navigate(['/not-found']);
     }
-    else this.router.navigate(['/not_found']);
+    else this.router.navigate(['/login']);
   }
 
 }
