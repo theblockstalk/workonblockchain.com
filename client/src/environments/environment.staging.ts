f31f791f8291f39c82a8b260d2f13278cd4e4cac
Jack Tanner <jack@jackandtheblockstalk.com>
2019-10-10 16:02:01 +0100

    changed google codes

--- client/src/environments/environment.staging.ts
+++ client/src/environments/environment.staging.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   backend_url: 'https://staging-api.workonblockchain.com/',
   frontend_url: 'https://staging.workonblockchain.com/',
-  google_client_id : '699068166900-h9dp76b47mh4biq33e8c31ju8tvfgvqj.apps.googleusercontent.com',
+  google_client_id : '991119316767-m1m39sjm106epol8nuceh6pq57btp28p.apps.googleusercontent.com',
   linkedin_id: '86q3xso6dxvymx',
   linkedin_redirect_url : 'https://staging.workonblockchain.com/linkedin-auth',
   google_redirect_url : 'https://staging.workonblockchain.com/google-auth',
