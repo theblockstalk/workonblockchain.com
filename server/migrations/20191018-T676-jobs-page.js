39cf78dab0801ede94f6c45ee4558bb88120b310
Tayyab Hussain <tayyab.hussain@mwanmobile.org>
2019-10-28 11:32:22 +0500

    fixed migration script error

--- server/migrations/20191018-T676-jobs-page.js
+++ server/migrations/20191018-T676-jobs-page.js
@@ -28,7 +28,7 @@ module.exports.up = async function() {
                 description : '',
                 created : timestamp,
                 modified : timestamp
-            },
+            };
             if (savedSearch.visa_needed) newJob.visa_needed = savedSearch.visa_needed;
             if (savedSearch.job_type) newJob.job_type = savedSearch.job_type;
             if (savedSearch.current_salary) newJob.expected_salary_min = savedSearch.current_salary;
